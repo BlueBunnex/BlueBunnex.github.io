fd71fb09b648f60e19cfdf7f7f34e052e2696b39
BlueBunnex <75032462+BlueBunnex@users.noreply.github.com>
2022-06-23 10:57:03 -0400

    support lightbox (test)

--- load.js
+++ load.js
@@ -1,3 +1,39 @@
+// Open the Modal
+function openModal() {
+    document.getElementById("myModal").style.display = "block";
+}
+
+// Close the Modal
+function closeModal() {
+    document.getElementById("myModal").style.display = "none";
+}
+
+var slideIndex = 1;
+
+// Next/previous controls
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+    var i;
+    var slides = document.getElementsByClassName("mySlides");
+
+    if (n > slides.length) {slideIndex = 1}
+    if (n < 1) {slideIndex = slides.length}
+
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+
+    slides[slideIndex-1].style.display = "block";
+}
+
 function loadTheThings() {
     var construct = "";
 
@@ -10,7 +46,7 @@ function loadTheThings() {
 
         construct += `
         <span class="card">
-            <span class="img-holder">
+            <span class="img-holder" onclick="openModal(); currentSlide(1);">
                 <span><i class="fa-solid fa-camera"></i> ` + value["images"].length + `</span>
                 <img src="` + value["images"][0] + `">
             </span>
@@ -29,7 +65,8 @@ var blogCards = [
         "description": "We are now hosting on Github, which makes updating blog posts very convenient!",
         "date": "06/20/2022",
         "images": [
-            "img/kitsu_.png"
+            "img/kitsu_.png",
+            "img/banner.png",
         ]
         //"page": "404.html"
     }

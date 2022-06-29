beb88a521920f80f029bf0256506c67a4bc74095
kitsumitsu <75032462+BlueBunnex@users.noreply.github.com>
2022-06-29 15:12:52 -0400

    now we're good to upload the content...

--- load.js
+++ load.js
@@ -94,11 +94,11 @@ var blogCards = [
     {
         "description": "First day of travel (to Germany on plane!!) and I already feel like death. I was very tired. Lots of cool photos though!",
         "date": "06/27/2022",
-        "image": "arrival_fumo.png",
+        "image": "img/arrival_fumo.png",
         "image-count": 18,
         "slides": [
             `
-            <a href="https://lh6.googleusercontent.com/6c_iEK6WX5WIec4dSs3RNTO7JHIs8RdqNCpCIE_g352iQchfhz-qd6QpWCi8d8QwdCw=w2400?source=screenshot.guru"> <img src="https://lh6.googleusercontent.com/6c_iEK6WX5WIec4dSs3RNTO7JHIs8RdqNCpCIE_g352iQchfhz-qd6QpWCi8d8QwdCw=w1200-h630-p" /></a>
+            <a href="https://lh6.googleusercontent.com/6c_iEK6WX5WIec4dSs3RNTO7JHIs8RdqNCpCIE_g352iQchfhz-qd6QpWCi8d8QwdCw=w2400?source=screenshot.guru"><img src="https://lh6.googleusercontent.com/6c_iEK6WX5WIec4dSs3RNTO7JHIs8RdqNCpCIE_g352iQchfhz-qd6QpWCi8d8QwdCw=w1200-h630-p" /></a>
             Fumo fumo fumo!
             `
         ]

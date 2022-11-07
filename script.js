9bb13c89d0bde63f2ddeadff1d4263bc1e16d031
kitsumitsu <75032462+BlueBunnex@users.noreply.github.com>
2022-11-07 15:33:04 -0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+var isEnglish = true;
+                
+function translate() {
+  isEnglish = !isEnglish;
+
+  alert(isEnglish);
+}
+
+// also make sure to set all Chinese to "display: none;" on page load

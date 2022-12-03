7d6312ad3d5c7b592f27a0a7a3b47463f1fdf466
kitsumitsu <75032462+BlueBunnex@users.noreply.github.com>
2022-12-03 12:57:52 -0500

    testing if switching between en and cn works
    I put ch last time I'm sowwwy

--- script.js
+++ script.js
@@ -1,15 +1,32 @@
-var isEnglish = true;
+var isEnglish = false;
                 
 function translatePage() {
   isEnglish = !isEnglish;
 
   //alert("you found a secret! " + isEnglish);
   
+  var enDisplay;
+  var cnDisplay;
+  
+  if (isEnglish) {
+    enDisplay = "block";
+    cnDisplay = "none";
+  } else {
+    enDisplay = "none";
+    cnDisplay = "block";
+  }
+  
   var en = document.getElementsByClassName("English");
   for (var i = 0; i < en.length; i++) {
-     en.item(i).style.display = "none";
+     en.item(i).style.display = enDisplay;
+  }
+  
+  var cn = document.getElementsByClassName("Chinese");
+  for (var i = 0; i < cn.length; i++) {
+     cn.item(i).style.display = cnDisplay;
   }
   
 }
 
 // also make sure to set all Chinese to "display: none;" on page load
+translatePage();

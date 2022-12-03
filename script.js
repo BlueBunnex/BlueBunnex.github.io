631a7b826141998f862e8ebab5b2dc50c7258956
kitsumitsu <75032462+BlueBunnex@users.noreply.github.com>
2022-12-03 12:49:38 -0500

    testing en to ch

--- script.js
+++ script.js
@@ -3,7 +3,13 @@ var isEnglish = true;
 function translatePage() {
   isEnglish = !isEnglish;
 
-  alert("you found a secret! " + isEnglish);
+  //alert("you found a secret! " + isEnglish);
+  
+  var en = document.getElementsByClassName("English");
+  for (var i = 0; i < en.length; i++) {
+     en.item(i).style.display = "none";
+  }
+  
 }
 
 // also make sure to set all Chinese to "display: none;" on page load

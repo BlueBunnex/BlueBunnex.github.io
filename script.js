25cef0a70fbab7dea496e3a3b918bb26d309b625
kitsumitsu <75032462+BlueBunnex@users.noreply.github.com>
2023-04-18 15:42:52 -0400

    add cookie memory for language

--- script.js
+++ script.js
@@ -1,21 +1,50 @@
-var isEnglish = false;
+// cookies helper functions
+
+function setCookie(cname, cvalue, exdays) {
+  const d = new Date();
+  d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
+  let expires = "expires="+d.toUTCString();
+  document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+}
+
+function getCookie(cname) {
+  let name = cname + "=";
+  let ca = document.cookie.split(';');
+  for(let i = 0; i < ca.length; i++) {
+    let c = ca[i];
+    while (c.charAt(0) == ' ') {
+      c = c.substring(1);
+    }
+    if (c.indexOf(name) == 0) {
+      return c.substring(name.length, c.length);
+    }
+  }
+  return "";
+}
+
+// page translation code
+
+var isEnglish = getCookie("language") === "English";
                 
 function translatePage() {
   isEnglish = !isEnglish;
-
-  //alert("you found a secret! " + isEnglish);
   
   var enDisplay;
   var cnDisplay;
   
   if (isEnglish) {
+    setCookie("language", "English", 7);
+    
     enDisplay = "block";
     cnDisplay = "none";
   } else {
+    setCookie("language", "Chinese", 7);
+    
     enDisplay = "none";
     cnDisplay = "block";
   }
   
+  // update page
   var en = document.getElementsByClassName("English");
   for (var i = 0; i < en.length; i++) {
      en.item(i).style.display = enDisplay;
@@ -25,5 +54,4 @@ function translatePage() {
   for (var i = 0; i < cn.length; i++) {
      cn.item(i).style.display = cnDisplay;
   }
-  
 }

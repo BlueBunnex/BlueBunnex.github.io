2c70aa2f62280fe27fde41d659edd69e7558091e
kitsumitsu <75032462+BlueBunnex@users.noreply.github.com>
2023-05-25 15:14:39 -0400

    Create characters.js
    also add enul data

---
+++ characters.js
@@ -0,0 +1,39 @@
+const characterInfo = {
+  'purdy': {
+      name: "Purdy",
+      img: "purdy.png",
+      facts: [
+          "Sarrath city, though located in the arid desert, often doesn't seem that way due to the extreme urbanization and green efforts."
+      ],
+      design: "A bottom heavy coffee-colored rabbit with short blonde hair and droopy rabbit ears. Her default outfit is a dark maroon crop-top sweater, clean white jean shorts and thigh highs, and thick silver bangles on her wrists. Resting :3 face.",
+      colors: [
+          "#3d1122",
+          "#b0664f",
+          "#f0ffff",
+          "#ffedab"
+      ],
+      paragraphs: [
+          "Growing up in the country on urban-dramas gave Purdy an almost insatiable idolatry of city life - guns, police, violence, love, <i>food,</i> filling out her heart and her figure alike. All through college before retiring for the night she would stare out from her verdant, earthly dorm room on a purpling sky, the buildings of the city just showing through the light of dusk. <i>Sarrath</i> city.",
+          "And then, she graduated. Though she had already frequently bought new clothes since her admission to college (she had jumped up a few sizes since then, after all), she decided she needed a change of style. A taxi later (she knew how to drive, but she had always seen the taxis on the screen) she flaunted herself from the mall newly changed and carrying a hefty bag in each hand, if not feeling a little overexposed.",
+          "As the sun set and the sky purpled just as it had when she was still in college, with six months of rent prepaid for in an apartment quickly discovered to be too cramped for a gal of her scale, a sharp aching in her heart cried out to her of homesickness (well, it might have been from the fat built up around her flabby chest, but we'll call it homesickness). From the twentieth floor the view sure was <i>different</i> from what she had grown accustommed to in the country, and—hey, is that a McDonalds?",
+      ],
+      table_data: {
+          Weight: "245lb"
+      }
+  },
+  'enul': {
+      name: "Ênul",
+      img: "kitsu_.png",
+      facts: [],
+      design: "widest brightest eyes, foxgirl, pale blue skin, thin, flat, wide hips/fat butt/calves, beret, 'office' skirt",
+      colors: [],
+      paragraphs: [
+        "(WIP) Ênul is frankly sick of hearing the word 'diagnosed.'",
+        "Her Findr profile lists her as 'energetic but sleepy.' She has since stopped using photos that had her lower half in them.",
+        "She reckons that if she found a man that would _ with her, then that would be nice. She told this to her doctor, but he just told her to remain cautious."
+      ],
+      table_data: {
+          Weight: "130lb"
+      }
+  }
+};

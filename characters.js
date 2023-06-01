3973e82b355a2c1df6fe8bed6363c99773308d56
kitsumitsu <75032462+BlueBunnex@users.noreply.github.com>
2023-06-01 13:34:02 -0400

    update purdy nice and good

--- characters.js
+++ characters.js
@@ -5,7 +5,7 @@ const characterInfo = {
       facts: [
           "Sarrath city, though located in the arid desert, often doesn't seem that way due to the extreme urbanization and green efforts."
       ],
-      design: "A bottom heavy coffee-colored rabbit with short blonde hair and droopy rabbit ears. Her default outfit is a dark maroon crop-top sweater, clean white jean shorts and thigh highs, and thick silver bangles on her wrists. Resting :3 face.",
+      design: "A shortstock dark-skinned woman with curt blonde hair. Her default outfit is a dark maroon crop-top sweater, clean white jean shorts and thigh highs, and thick silver bangles on her wrists. Resting :3 face.",
       colors: [
           "#3d1122",
           "#b0664f",
@@ -13,9 +13,9 @@ const characterInfo = {
           "#ffedab"
       ],
       paragraphs: [
-          "Growing up in the country on urban-dramas gave Purdy an almost insatiable idolatry of city life - guns, police, violence, love, <i>food,</i> filling out her heart and her figure alike. All through college before retiring for the night she would stare out from her verdant, earthly dorm room on a purpling sky, the buildings of the city just showing through the light of dusk. <i>Sarrath</i> city.",
-          "And then, she graduated. Though she had already frequently bought new clothes since her admission to college (she had jumped up a few sizes since then, after all), she decided she needed a change of style. A taxi later (she knew how to drive, but she had always seen the taxis on the screen) she flaunted herself from the mall newly changed and carrying a hefty bag in each hand, if not feeling a little overexposed.",
-          "As the sun set and the sky purpled just as it had when she was still in college, with six months of rent prepaid for in an apartment quickly discovered to be too cramped for a gal of her scale, a sharp aching in her heart cried out to her of homesickness (well, it might have been from the fat built up around her flabby chest, but we'll call it homesickness). From the twentieth floor the view sure was <i>different</i> from what she had grown accustommed to in the country, and—hey, is that a McDonalds?",
+          "I only have sentence segments so lore is still WIP"
+          "...she flaunted herself from the mall newly changed and carrying a hefty bag in each hand, if not feeling a little overexposed, the flabby folds of her belly muffin-topping out from above her jeans.",
+          "...with six months of rent prepaid for in an apartment quickly discovered to be too cramped for a gal of her scale... (hallway-clogging?)",
       ],
       table_data: {
           Weight: "245lb"

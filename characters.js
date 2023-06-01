acbaede778c17888c2c74e96677bf8e022b62129
kitsumitsu <75032462+BlueBunnex@users.noreply.github.com>
2023-06-01 15:11:00 -0400

    pizza? pizza!

--- characters.js
+++ characters.js
@@ -3,9 +3,10 @@ const characterInfo = {
       name: "Purdy",
       img: "purdy.png",
       facts: [
-          "Her city of Sarrath is located in the arid desert. She works for the Sarrath Green Development Company (SGDC), though her position in management has her working inside most of the time."
+          "Her city of Sarrath is located in the arid desert. She works for the Sarrath Green Development Company (SGDC), though her position in management has her working inside most of the time.",
+          "Her favourite food is the thick-crust curry pizza from Izza's Pizzas."
       ],
-      design: "A shortstock dark-skinned woman with curt blonde hair. She's peppy, bright, and comfortable being overweight. Resting :3 face. Her default outfit is a dark maroon crop-top sweater, clean white jean shorts and thigh highs, and thick silver bangles on her wrists. She wears a suit to work, pencil skirt, and heels.",
+      design: "A shortstock dark-skinned woman with curt blonde hair. She's peppy, bright, and comfortable being overweight. Resting :3 face. Her default outfit is a dark maroon crop-top sweater, clean white jean shorts and thigh highs, and thick silver bangles on her wrists. She wears a suit, pencil skirt, and heels to work.",
       colors: [
           "#3d1122",
           "#b0664f",

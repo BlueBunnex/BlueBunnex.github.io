ee5cc46418710abac6d23e724487c026531fd015
kitsumitsu <75032462+BlueBunnex@users.noreply.github.com>
2023-06-15 17:11:04 -0400

    update pencil skirt info

--- characters.js
+++ characters.js
@@ -3,19 +3,19 @@ const characterInfo = {
       name: "Julianna Lotusheart",
       img: null,
       facts: [],
-      design: "A purple goth witch tigress. She's chill and moody.",
+      design: "A purple goth witch tigress. She's chill and moody. She wears a black loose croptop, pencil skirt (uh not yet) and some other stuff.",
       colors: [],
       paragraphs: [],
       table_data: {}
   },
   'purdy': {
       name: "Purdy",
-      img: "purdy_thicks.png",
+      img: "purdy.png",
       facts: [
           "Her city of Sarrath is located in the arid desert. She works for the Sarrath Green Development Company (SGDC), though her position in management has her working inside most of the time.",
           "Her favourite food is the thick-crust curry pizza from Izza's Pizzas."
       ],
-      design: "A shortstock dark-skinned woman with curt blonde hair. She's peppy, bright, and comfortable being overweight. Resting :3 face. Her default outfit is a dark maroon crop-top sweater, clean white jean shorts and thigh highs, and thick silver bangles on her wrists. She wears a suit, pencil skirt, and heels to work.",
+      design: "A shortstock dark-skinned woman with curt blonde hair. She's peppy, bright, and comfortable being overweight. Resting :3 face. Her default outfit is a dark maroon crop-top sweater, clean white jean shorts and thigh highs, and thick silver bangles on her wrists.",
       colors: [
           "#3d1122",
           "#b0664f",

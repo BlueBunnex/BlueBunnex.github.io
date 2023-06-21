fa6f1f3184ed1232dbba00a702c4eef1d5619230
kitsumitsu <75032462+BlueBunnex@users.noreply.github.com>
2023-06-20 20:21:27 -0400

    Cacao is gonna be my comfort oc

--- characters.js
+++ characters.js
@@ -1,60 +1,17 @@
 const characterInfo = {
-  'julia': {
-      name: "Julianna Lotusheart",
-      tagline: "goth witch tigress",
-      img: "julia.png",
+  'cacao': {
+      name: "Cacao",
+      tagline: "",
+      img: null,
       facts: [],
-      design: "She's sleepy and moody, though chill once you get to know her. She wears a black loose croptop, pencil skirt (uh not yet) and some other stuff.",
+      design: 'A tan wolf that is a little pudgey.',
       colors: [],
       paragraphs: [],
-      table_data: {}
-  },
-  'purdy': {
-      name: "Purdy",
-      tagline: "peppy city gal",
-      img: "purdy.png",
-      facts: [
-          "Her city of Sarrath is located in the arid desert. She works for the Sarrath Green Development Company (SGDC), though her position in management has her working inside most of the time.",
-          "Her favourite food is the thick-crust curry pizza from Izza's Pizzas."
-      ],
-      design: "A shortstock dark-skinned woman with curt blonde hair. She's peppy, bright, and comfortable being overweight. Resting :3 face. Her default outfit is a dark maroon crop-top sweater, clean white jean shorts and thigh highs, and thick silver bangles on her wrists.",
-      colors: [
-          "#3d1122",
-          "#b0664f",
-          "#f0ffff",
-          "#ffedab"
-      ],
-      paragraphs: [],
-      table_data: {}
-  },
-  'sam': {
-      name: "Samantha",
-      tagline: "big water dragon",
-      img: "big_samantha.png",
-      facts: [
-          "She has the ability to purify any liquid that she consumes into crystal clear water, then spit it out again like a hose. For this purpose, her belly, breasts, and hips can stretch to accomodate an enourmous amount of water.",
-          "As a dragon, she has the instinctive compulsion to amass and maintain a horde of treasure. Living in human society, this results in a large pile of trinkets shoved in one corner of her room."
-      ],
-      design: 'A fluffy water-bloated dragon that breaths water. Heavy deep blue lipstick and eyeshadow, turquoise scaley belly and tail, blue furry arms and legs. Currently wears only a red swimsuit with daisies.',
-      colors: [
-        "#cfffe1",
-        "#81d6e3",
-        "#222b5e"
-      ],
-      paragraphs: [
-          "Her personality is a switch: If the second person is submissive then she rises to fill the role of leader and acts very confidently, but if the second person is dominant then she remains mellow and coy. If she's with more than one person who are all submissive, it may take longer for her confidence to rise enough to lead the group.",
-          "Being so tall, Sam has trouble fitting into most buildings not expressely designed to accomodate dragons, but that hasn't stopped her from trying yet—always convincing herself that she's small enough for the door, only for her massive butt to wedge into the frame right as she thinks she's through.",
-          'Contrary to her appearance she is strong, tough, and agile, for she exercises moderately and eats very well.'
-      ],
       table_data: {
-          Gender: "Female",
+          Identity: "Woman",
           Orientation: "Pansexual",
-          Weight: "varies based on water carried",
-          Height: "8'1\""
+          Weight: "Over",
+          Height: "Short"
       }
   }
 };
-// "...she flaunted herself from the mall newly changed and carrying a hefty bag in each hand, if not feeling a little overexposed, the flabby folds of her belly muffin-topping out from above her jeans.",
-// "...with six months of rent prepaid for in an apartment quickly discovered to be too cramped for a hallway-clogging gal like herself..."
-
-// "How am I supposed to rule when my hiPS ARE BIGGER THAN MY HOARD!?"

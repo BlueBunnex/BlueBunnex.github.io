9fbef77a4e0424e88abec81a6ba25e9caa50f472
kitsumitsu <75032462+BlueBunnex@users.noreply.github.com>
2023-06-20 21:30:59 -0400

    cacao likes horror

--- characters.js
+++ characters.js
@@ -4,7 +4,8 @@ const characterInfo = {
       tagline: "snuggly chocolate wolf",
       img: null,
       facts: [
-        "When she has trouble sleeping, she likes a little hot chocolate and snuggles in bed to help settle in."
+        "When she has trouble sleeping, she likes a little hot chocolate and snuggles in bed to help settle in.",
+        "She loves all things horror. Games, movies, even art. Nevermind she doesn't have the guts to enjoy any of it without someone to grab onto right beside her."
       ],
       design: 'A tan wolf that is a little pudgey. Her belly and cheeks are extra fluffy. She has purple-yellow heterochromia which is mirrored by the bows in her hair.',
       colors: [],

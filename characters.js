a4ace7ecbbaa086e1adfac21ff08c3f41a064ba1
kitsumitsu <75032462+BlueBunnex@users.noreply.github.com>
2023-06-22 23:16:52 -0400

    add more important details to cacao's design

--- characters.js
+++ characters.js
@@ -7,11 +7,13 @@ const characterInfo = {
           "When she has trouble sleeping, she likes a little midnight hot chocolate.",
           "Her favourite food is a fried chicken sandwich with pickles and mayo."
       ],
-      design: "A tan~brown wolf that is a little overweight. She has purple-yellow heterochromia. She is boisterous, well-intentioned, and colorful in nature, but gets ticked off when she feels like she's being pushed around.",
+      design: "A brown wolf that is a little overweight. She has purple-yellow heterochromia, and her eye shine are little hearts. Her hands, feet, and tail features a bubble pattern as it transitions from brown to tan. She (typically) wears a red hoodie over a tan tee that reads ROFL across the front.",
       colors: [
           "#9c7767"
       ],
-      paragraphs: [],
+      paragraphs: [
+          "(WIP paragraphs) She is boisterous, well-intentioned, and colorful in nature, but gets ticked off when she feels like she's being pushed around."
+      ],
       table_data: {
           Identity: "Woman",
           Orientation: "Straight",

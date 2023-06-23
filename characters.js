81c70b19ba6a9d9ca52acfe1fa62b7c23c1c4428
kitsumitsu <75032462+BlueBunnex@users.noreply.github.com>
2023-06-22 23:22:50 -0400

    fix grammar

--- characters.js
+++ characters.js
@@ -7,12 +7,12 @@ const characterInfo = {
           "When she has trouble sleeping, she likes a little midnight hot chocolate.",
           "Her favourite food is a fried chicken sandwich with pickles and mayo."
       ],
-      design: "A brown wolf that is a little overweight. She has purple-yellow heterochromia, and her eye shine are little hearts. Her hands, feet, and tail features a bubble pattern as it transitions from brown to tan. She (typically) wears a red hoodie over a tan tee that reads ROFL across the front.",
+      design: "A brown wolf that is a little overweight. She has purple-yellow heterochromia, and her eye shine forms little hearts. Her hands, feet, and tail feature a bubble pattern as it transitions from brown to tan. She (typically) wears a red hoodie over a tan tee that reads ROFL across the front.",
       colors: [
           "#9c7767"
       ],
       paragraphs: [
-          "(WIP paragraphs) She is boisterous, well-intentioned, and colorful in nature, but gets ticked off when she feels like she's being pushed around."
+          "Cocoa is boisterous, well-intentioned, and colorful in nature, but gets ticked off when she feels like she's being pushed around. She enjoys going on jogs and posing for stills."
       ],
       table_data: {
           Identity: "Woman",

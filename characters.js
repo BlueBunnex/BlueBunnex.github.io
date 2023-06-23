443d814c7425a486c795b781507326892435b0dd
kitsumitsu <75032462+BlueBunnex@users.noreply.github.com>
2023-06-22 23:27:38 -0400

    update grammar (again)

--- characters.js
+++ characters.js
@@ -4,10 +4,10 @@ const characterInfo = {
       tagline: "snuggly chocolate wolf",
       img: "cacao0.png",
       facts: [
-          "When she has trouble sleeping, she likes a little midnight hot chocolate.",
+          "When she has trouble sleeping, she likes a little midnight hot chocolate. Or when it's cold out. Or any time, really.",
           "Her favourite food is a fried chicken sandwich with pickles and mayo."
       ],
-      design: "A brown wolf that is a little overweight. She has purple-yellow heterochromia, and her eye shine forms little hearts. Her hands, feet, and tail feature a bubble pattern as it transitions from brown to tan. She (typically) wears a red hoodie over a tan tee that reads ROFL across the front.",
+      design: "A brown wolf that is a little overweight. She has purple-yellow heterochromia, and her eye shine forms little hearts. Her hands, feet, and tail feature a bubble pattern as they transition from brown to tan. She (typically) wears a red hoodie over a tan tee that reads ROFL across the front.",
       colors: [
           "#9c7767"
       ],

04f63b5348e29ed377c173195307e94f6536c875
kitsumitsu <75032462+BlueBunnex@users.noreply.github.com>
2023-07-05 16:04:57 -0400

    Create auto.js

---
+++ auto.js
@@ -0,0 +1,5 @@
+// home button
+document.body.innerHTML += `<a onclick="location.href = 'index.html';" style="cursor: pointer; padding: 10px; border-radius: 10px; position: absolute; top: 10px; left: 10px; background: var(--ten-dark); color: var(--thirty);">
+  <i class="fa-solid fa-arrow-left"></i>
+  <i class="fa-solid fa-house"></i>
+</a>`;

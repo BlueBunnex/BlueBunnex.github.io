d9d4670b15f881c6eef8fc4b8ffc96cf5ee1d140
kitsumitsu <75032462+BlueBunnex@users.noreply.github.com>
2023-07-05 16:47:32 -0400

    auto function

--- auto.js
+++ auto.js
@@ -1,15 +1,15 @@
-// potentially put this script in head, then call various functions on body onload depending on page needs, but w/e
-
-// home button
-document.body.innerHTML += `
-<a onclick="location.href = 'index.html';" style="transition-duration: 0.2s; cursor: pointer; padding: 10px; border-radius: 10px; position: absolute; top: 10px; left: 10px; background: var(--ten-dark); color: var(--thirty);">
-  <i class="fa-solid fa-arrow-left"></i>
-  <i class="fa-solid fa-house"></i>
-</a>`;
-
-// copyright at bottom
-document.body.innerHTML += `<!-- copyright -->
-<br><br>
-<span style="text-align: center; color: var(--ten-dark); font-size: 12px; width: 100%; position: absolute; bottom: 10px; transform: translateX(-50%);">
-    © 2023 Kitsumitsu46. All Rights Reserved. <i class="fa-solid fa-heart" style="color: pink;"></i>
-</span>`;
+function auto() {
+  // home button
+  document.body.innerHTML += `
+  <a onclick="location.href = 'index.html';" style="transition-duration: 0.2s; cursor: pointer; padding: 10px; border-radius: 10px; position: absolute; top: 10px; left: 10px; background: var(--ten-dark); color: var(--thirty);">
+    <i class="fa-solid fa-arrow-left"></i>
+    <i class="fa-solid fa-house"></i>
+  </a>`;
+  
+  // copyright at bottom
+  document.body.innerHTML += `<!-- copyright -->
+  <br><br>
+  <span style="text-align: center; color: var(--ten-dark); font-size: 12px; width: 100%; position: absolute; bottom: 10px; transform: translateX(-50%);">
+      © 2023 Kitsumitsu46. All Rights Reserved. <i class="fa-solid fa-heart" style="color: pink;"></i>
+  </span>`;
+}

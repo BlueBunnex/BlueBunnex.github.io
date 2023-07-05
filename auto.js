7058b44d9ac95ba6e7952aa66ec0c5b1fe503122
kitsumitsu <75032462+BlueBunnex@users.noreply.github.com>
2023-07-05 16:21:17 -0400

    add transition duration to home thing

--- auto.js
+++ auto.js
@@ -1,5 +1,6 @@
 // home button
-document.body.innerHTML += `<a onclick="location.href = 'index.html';" style="cursor: pointer; padding: 10px; border-radius: 10px; position: absolute; top: 10px; left: 10px; background: var(--ten-dark); color: var(--thirty);">
+document.body.innerHTML += `
+<a onclick="location.href = 'index.html';" style="transition-duration: 0.2s; cursor: pointer; padding: 10px; border-radius: 10px; position: absolute; top: 10px; left: 10px; background: var(--ten-dark); color: var(--thirty);">
   <i class="fa-solid fa-arrow-left"></i>
   <i class="fa-solid fa-house"></i>
 </a>`;

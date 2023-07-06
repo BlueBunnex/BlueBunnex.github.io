4dfd2e41dde0a3f3d3bac2a94529d168a052bf41
kitsumitsu <75032462+BlueBunnex@users.noreply.github.com>
2023-07-06 13:20:25 -0400

    add unknown char

--- characters.js
+++ characters.js
@@ -1,26 +1,46 @@
 const characterInfo = {
-  'cacao': {
-      name: "Cacao",
-      tagline: "snuggly chocolate wolf",
-      img: "cacao0.png",
-      reference: "cacao_ref.png",
-      facts: [
-          "When she has trouble sleeping, she likes a little midnight hot chocolate. Or when it's cold out. Or any time, really.",
-          "Her favourite food is a fried chicken sandwich with pickles and mayo."
-      ],
-      design: "A brown wolf that is a little overweight. She has purple-yellow heterochromia, and her eye shine forms little hearts. Her hands, feet, and tail feature a bubble pattern as they transition from brown to tan. She (typically) wears a red hoodie over a tan tee that reads ROFL across the front.",
-      colors: [
-          "#9c7767"
-      ],
-      paragraphs: [
-          "Cocoa is boisterous, well-intentioned, and colorful in nature, but gets ticked off when she feels like she's being pushed around. She enjoys going on jogs and posing for stills."
-      ],
-      table_data: {
-          Species: "Wolf",
-          Identity: "Woman",
-          Orientation: "Straight",
-          Weight: "Over",
-          Height: "Short"
-      }
-  }
+    'cacao': {
+        name: "Cacao",
+        tagline: "snuggly chocolate wolf",
+        img: "cacao0.png",
+        reference: "cacao_ref.png",
+        facts: [
+            "When she has trouble sleeping, she likes a little midnight hot chocolate. Or when it's cold out. Or any time, really.",
+            "Her favourite food is a fried chicken sandwich with pickles and mayo."
+        ],
+        design: "A brown wolf that is a little overweight. She has purple-yellow heterochromia, and her eye shine forms little hearts. Her hands, feet, and tail feature a bubble pattern as they transition from brown to tan. She (typically) wears a red hoodie over a tan tee that reads ROFL across the front.",
+        colors: [
+            "#9c7767"
+        ],
+        paragraphs: [
+            "Cocoa is boisterous, well-intentioned, and colorful in nature, but gets ticked off when she feels like she's being pushed around. She enjoys going on jogs and posing for stills."
+        ],
+        table_data: {
+            Species: "Wolf",
+            Identity: "Woman",
+            Orientation: "Straight",
+            Weight: "Over",
+            Height: "Short"
+        }
+    },
+    'unknown': {
+        name: "unknown",
+        tagline: "b",
+        img: null,
+        reference: null,
+        facts: [],
+        design: "Pink hair, Gatorade blue mouth and inner fox ear.",
+        colors: [
+            "#3639ff",
+            "#ff96c4"
+        ],
+        paragraphs: [],
+        table_data: {
+            Species: "Human? Fox? dunno",
+            Identity: "Woman",
+            Orientation: "Asexual, Romantic",
+            Weight: "Over",
+            Height: "Tall"
+        }
+    }
 };

b8b72900fd84258eeced4a8262d04c30cff6adca
kitsumitsu <75032462+BlueBunnex@users.noreply.github.com>
2023-07-07 12:42:08 -0400

    update to match bcp info

--- characters.js
+++ characters.js
@@ -8,13 +8,13 @@ const characterInfo = {
             "When she has trouble sleeping, she likes a little midnight hot chocolate. Or when it's cold out. Or any time, really.",
             "Her favourite food is a fried chicken sandwich with pickles and mayo."
         ],
-        design: "A brown wolf that is a little overweight. She has purple-yellow heterochromia, and her eye shine forms little hearts. Her hands, feet, and tail feature a bubble pattern as they transition from brown to tan. She (typically) wears a red hoodie over a tan tee that reads ROFL across the front.",
+        body: "A brown wolf that is a little overweight. She has purple-yellow heterochromia, and her eye shine forms little hearts. Her hands, feet, and tail feature a bubble pattern as they transition from brown to tan.",
+        clothing: "She (typically) wears a red hoodie over a tan tee that reads ROFL across the front.",
+        personality: "Cocoa is boisterous, well-intentioned, and colorful in nature, but gets ticked off when she feels like she's being pushed around.",
         colors: [
             "#9c7767"
         ],
-        paragraphs: [
-            "Cocoa is boisterous, well-intentioned, and colorful in nature, but gets ticked off when she feels like she's being pushed around. She enjoys going on jogs and posing for stills."
-        ],
+        paragraphs: [],
         table_data: {
             Species: "Wolf",
             Identity: "Woman",
@@ -29,7 +29,9 @@ const characterInfo = {
         img: "bonnie_ex.png",
         reference: null,
         facts: [],
-        design: "Pink hair, Gatorade blue mouth.",
+        body: "Pink hair, Gatorade blue mouth.",
+        clothing: "Tube top.",
+        personality: "idk",
         colors: [
             "#3639ff",
             "#ff96c4"

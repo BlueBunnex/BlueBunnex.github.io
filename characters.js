0c969b7345f71c105f2249904d1e38473bc01a90
kitsumitsu <75032462+BlueBunnex@users.noreply.github.com>
2023-07-09 11:08:30 -0400

    update clothing wip

--- characters.js
+++ characters.js
@@ -30,7 +30,7 @@ const characterInfo = {
         reference: null,
         facts: [],
         body: "Pink hair, blue mouth. She has a muffintop while wearing tight clothes.",
-        clothing: "I'm still workshopping what her 'default' clothing will be. I'm thinking about having her wear an apron though...",
+        clothing: "I'm still workshopping what her 'default' clothing will be. I'm thinking about having her wear an apron though, tied off at the waist, and with a darker pink/red undershirt.",
         personality: "Upbeat sometimes, and sleepy other times.",
         colors: [
             "#3639ff",

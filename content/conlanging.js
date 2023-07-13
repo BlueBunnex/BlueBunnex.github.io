16c9c84cb509dd90eaeed5cc4379122c4e7ed514
kitsumitsu <75032462+BlueBunnex@users.noreply.github.com>
2023-07-13 18:39:51 -0400

    Update conlanging.js

--- content/conlanging.js
+++ content/conlanging.js
@@ -1,132 +1,137 @@
 const conlanging = `
 
-<p>Eventually I'll have it set up in a way so that it is ordered to be easily learned step by step, but w/e</p>
-
-<h2>Phonology</h2>
-<table style="width: 100%; text-align: center;">
-    <tbody>
-        <tr>
-            <th></th>
-            <th>Labial</th>
-            <th>Alveolar</th>
-            <th>Palatal</th>
-            <th>Velar</th>
-            <th>Glottal</th>
-        </tr>
-        <tr>
-            <th>Nasal</th>
-            <td>m <i>m</i></td>
-            <td>n <i>n</i></td>
-            <td></td>
-            <td>ŋ <i>ng</i></td>
-            <td></td>
-        </tr>
-        <tr>
-            <th>Plosive</th>
-            <td>p <i>p</i></td>
-            <td>t <i>t</i></td>
-            <td></td>
-            <td>k <i>k</i></td>
-            <td>ʔ <i>q</i></td>
-        </tr>
-        <tr>
-            <th>Affricate</th>
-            <td></td>
-            <td>ts <i>c</i></td>
-            <td>tɕ <i>ch</i></td>
-            <td></td>
-            <td></td>
-        </tr>
-        <tr>
-            <th>Fricative</th>
-            <td></td>
-            <td>s <i>s</i></td>
-            <td>ɕ <i>sh</i></td>
-            <td>χ <i>kh</i></td>
-            <td>h <i>h</i></td>
-        </tr>
-        <tr>
-            <th>Liquid</th>
-            <td>w <i>w</i></td>
-            <td></td>
-            <td>j <i>j</i></td>
-            <td>ʀ <i>r</i></td>
-            <td></td>
-        </tr>
-    </tbody>
-</table>
-
-<table style="width: 50%; text-align: center;">
-    <tbody>
-        <tr>
-            <th></th>
-            <th>Front</th>
-            <th>Central</th>
-            <th>Back</th>
-        </tr>
-        <tr>
-            <th>Close</th>
-            <td>i <i>i</i></td>
-            <td></td>
-            <td>u <i>u</i></td>
-        </tr>
-        <tr>
-            <th>Mid</th>
-            <td></td>
-            <td>ə <i>e</i></td>
-            <td></td>
-        </tr>
-        <tr>
-            <th>Open</th>
-            <td></td>
-            <td>a <i>a</i></td>
-            <td></td>
-        </tr>
-    </tbody>
-</table>
-
-<ol>
-    <h3>Notes</h3>
-    <li>CVC</li>
-</ol>
-
-<hr>
-    
-<h2>Lexicon</h2>
-<ul class="lefted-border">
-    <h3>Pronouns</h3>
-</ul>
-
-<ul class="lefted-border">
-    <h3>Nouns</h3>
-    <li>juche / person, human-being</li>
-    <li>ran</li>
-    <li>pan</li>
-</ul>
-
-<ul class="lefted-border">
-    <h3>Postpositions</h3>
-    <li>ep / indicates the direct object (Accusative)</li>
-</ul>
-
-<ul class="lefted-border">
-    <h3>Verbs</h3>
-    <li>chit</li>
-</ul>
-
-<ul class="lefted-border">
-    <h3>Finals</h3>
-    <li>cau</li>
-</ul>
-
-<hr>
-
-<h2>Reference Grammar</h2>
-head-final, SOV
-
-<hr>
-
-Juche ep pan chit cau.<br>
-S pos O V final
+<div class="section">
+    <h1>the limalh</h1>
+    <div>
+        <p>Eventually I'll have it set up in a way so that it is ordered to be easily learned step by step, but w/e</p>
+        
+        <h2>Phonology</h2>
+        <table style="width: 100%; text-align: center;">
+            <tbody>
+                <tr>
+                    <th></th>
+                    <th>Labial</th>
+                    <th>Alveolar</th>
+                    <th>Palatal</th>
+                    <th>Velar</th>
+                    <th>Glottal</th>
+                </tr>
+                <tr>
+                    <th>Nasal</th>
+                    <td>m <i>m</i></td>
+                    <td>n <i>n</i></td>
+                    <td></td>
+                    <td>ŋ <i>ng</i></td>
+                    <td></td>
+                </tr>
+                <tr>
+                    <th>Plosive</th>
+                    <td>p <i>p</i></td>
+                    <td>t <i>t</i></td>
+                    <td></td>
+                    <td>k <i>k</i></td>
+                    <td>ʔ <i>q</i></td>
+                </tr>
+                <tr>
+                    <th>Affricate</th>
+                    <td></td>
+                    <td>ts <i>c</i></td>
+                    <td>tɕ <i>ch</i></td>
+                    <td></td>
+                    <td></td>
+                </tr>
+                <tr>
+                    <th>Fricative</th>
+                    <td></td>
+                    <td>s <i>s</i></td>
+                    <td>ɕ <i>sh</i></td>
+                    <td>χ <i>kh</i></td>
+                    <td>h <i>h</i></td>
+                </tr>
+                <tr>
+                    <th>Liquid</th>
+                    <td>w <i>w</i></td>
+                    <td></td>
+                    <td>j <i>j</i></td>
+                    <td>ʀ <i>r</i></td>
+                    <td></td>
+                </tr>
+            </tbody>
+        </table>
+        
+        <table style="width: 50%; text-align: center;">
+            <tbody>
+                <tr>
+                    <th></th>
+                    <th>Front</th>
+                    <th>Central</th>
+                    <th>Back</th>
+                </tr>
+                <tr>
+                    <th>Close</th>
+                    <td>i <i>i</i></td>
+                    <td></td>
+                    <td>u <i>u</i></td>
+                </tr>
+                <tr>
+                    <th>Mid</th>
+                    <td></td>
+                    <td>ə <i>e</i></td>
+                    <td></td>
+                </tr>
+                <tr>
+                    <th>Open</th>
+                    <td></td>
+                    <td>a <i>a</i></td>
+                    <td></td>
+                </tr>
+            </tbody>
+        </table>
+        
+        <ol>
+            <h3>Notes</h3>
+            <li>CVC</li>
+        </ol>
+        
+        <hr>
+            
+        <h2>Lexicon</h2>
+        <ul class="lefted-border">
+            <h3>Pronouns</h3>
+        </ul>
+        
+        <ul class="lefted-border">
+            <h3>Nouns</h3>
+            <li>juche / person, human-being</li>
+            <li>ran</li>
+            <li>pan</li>
+        </ul>
+        
+        <ul class="lefted-border">
+            <h3>Postpositions</h3>
+            <li>ep / indicates the direct object (Accusative)</li>
+        </ul>
+        
+        <ul class="lefted-border">
+            <h3>Verbs</h3>
+            <li>chit</li>
+        </ul>
+        
+        <ul class="lefted-border">
+            <h3>Finals</h3>
+            <li>cau</li>
+        </ul>
+        
+        <hr>
+        
+        <h2>Reference Grammar</h2>
+        head-final, SOV
+        
+        <hr>
+        
+        Juche ep pan chit cau.<br>
+        S pos O V final
+    </div>
+</div>
 
 `;

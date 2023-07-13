29b818ee9b99adfde820ac5a465265a9c50e3ac1
kitsumitsu <75032462+BlueBunnex@users.noreply.github.com>
2023-07-13 18:12:32 -0400

    lim

--- content/conspecies.js
+++ content/conspecies.js
@@ -1,12 +1,19 @@
 const conspecies = `
-<img src="img/limalh_thing.png" width="100%">
-<br>
-Limalh [li.maʎ] come with lots of variants at varying rarities that I will come up with later (very cool)
-<br>
-<br>
-tail varieties<br>
-<strong style="background: green;">common</strong> bulb, long<br>
-<strong style="background: blue;">uncommon</strong> split end, spade<br>
-<strong style="background: purple;">rare</strong> double, loop<br>
-<strong style="background: red;">very rare</strong> ??
+
+<div class="section">
+    <h1>the limalh</h1>
+    <div>
+        <img src="img/limalh_thing.png" width="100%">
+        <br>
+        Limalh [li.maʎ] come with lots of variants at varying rarities that I will come up with later (very cool)
+        <br>
+        <br>
+        tail varieties<br>
+        <strong style="background: green;">common</strong> bulb, long<br>
+        <strong style="background: blue;">uncommon</strong> split end, spade<br>
+        <strong style="background: purple;">rare</strong> double, loop<br>
+        <strong style="background: red;">very rare</strong> ??
+    </div>
+</div>
+
 `;

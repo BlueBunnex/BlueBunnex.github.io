f627eb1bcce0471157feaa2a37878633ec00a985
kitsumitsu <75032462+BlueBunnex@users.noreply.github.com>
2023-07-13 21:44:06 -0400

    maybe procedural?

--- content/universe.js
+++ content/universe.js
@@ -1,16 +1,21 @@
 const universe = `
 
-<div id="universe-insert">
-    <div class="section">
-        <h1>cacao</h1>
-        <div>
-    
-        </div>
-    </div>
-</div>
+<div id="universe-insert"></div>
 
+<script src="characters.js"></script>
 <script>
     let in = document.getElementById("universe-insert");
+
+    for (const prop in characterInfo) {
+
+        in.innerHTML += \`<div class="section">
+            <h1>\` + characterInfo[prop]['name'] + \`</h1>
+            <div>
+        
+            </div>
+        </div>\`;
+        
+    }
 </script>
 
 `;

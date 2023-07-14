077cb3f08755c3e1da1c12f6e3e6ea0e690f7a1a
kitsumitsu <75032462+BlueBunnex@users.noreply.github.com>
2023-07-13 21:47:59 -0400

    burger test

--- content/universe.js
+++ content/universe.js
@@ -8,6 +8,8 @@ const universe = `
 
     for (const prop in characterInfo) {
 
+        alert(characterInfo[prop]['name'] + " likes burgers");
+
         in.innerHTML += \`<div class="section">
             <h1>\` + characterInfo[prop]['name'] + \`</h1>
             <div>

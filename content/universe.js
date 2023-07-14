aa90cb9804d5198fd783399ed7e9dd5b969b10c1
kitsumitsu <75032462+BlueBunnex@users.noreply.github.com>
2023-07-13 22:09:37 -0400

    this WILL work

--- content/universe.js
+++ content/universe.js
@@ -1,22 +1,16 @@
-const universe = "<div id='universe-insert' onload='universeInsert();'></div>";
+let construct = "";
 
-function universeInsert() {
+for (const prop in characterInfo) {
 
-    alert("bacon test");
-    
-    let insert = document.getElementById("universe-insert");
-    
-    for (const prop in characterInfo) {
-    
-        //alert(characterInfo[prop]['name'] + " likes burgers");
-    
-        insert.innerHTML += `<div class="section">
-            <h1>` + characterInfo[prop]['name'] + `</h1>
-            <div>
-        
-            </div>
-        </div>`;
-        
-    }
+    //alert(characterInfo[prop]['name'] + " likes burgers");
 
+    construct += `<div class="section">
+        <h1>` + characterInfo[prop]['name'] + `</h1>
+        <div>
+    
+        </div>
+    </div>`;
+    
 }
+
+const universe = construct;

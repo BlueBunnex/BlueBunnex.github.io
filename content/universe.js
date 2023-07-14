009298b892bd1170f99b74aea897364137e46fdb
kitsumitsu <75032462+BlueBunnex@users.noreply.github.com>
2023-07-14 18:51:29 -0400

    hopefully this works

--- content/universe.js
+++ content/universe.js
@@ -3,14 +3,25 @@ let construct = "";
 for (const prop in characterInfo) {
 
     let char = characterInfo[prop];
+
+    // open divs and create header
+    construct += "<div class='section'>";
+    construct += "<h1>` + char['name'] + `</h1>";
+    construct += "<div>";
+
+    // add tagline
+    construct += "<p>" + char['tagline'] + "</p>";
     
-    construct += `<div class="section">
-        <h1>` + char['name'] + `</h1>
-        <div>
-            <img src=/img/universe/` + char['imgs'][0] + `>
-            <p>` + char['tagline'] + `</p>
-        </div>
-    </div>`;
+    // add image
+    construct += "<img src='/img/universe/" + char['imgs'][0] + "' style='width: 320px;'>";
+
+    // add paragraphs
+    for (const para in char['paragraphs']) {
+        construct += "<p>" + para + "</p>";
+    }
+
+    // close divs
+    construct += "</div></div>";
     
 }
 

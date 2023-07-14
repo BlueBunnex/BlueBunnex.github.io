5b441f793da1b32b2799c6f3c167cf9ea431a78f
kitsumitsu <75032462+BlueBunnex@users.noreply.github.com>
2023-07-13 21:34:57 -0400

    Create universe.js

---
+++ content/universe.js
@@ -0,0 +1,16 @@
+const universe = `
+
+<div id="universe-insert">
+    <div class="section">
+        <h1>cacao</h1>
+        <div>
+    
+        </div>
+    </div>
+</div>
+
+<script>
+    let in = document.getElementById("universe-insert");
+</script>
+
+`;

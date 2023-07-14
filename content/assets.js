2cd7721d5d07a37095a73f2a58cfaf88a949488c
kitsumitsu <75032462+BlueBunnex@users.noreply.github.com>
2023-07-13 20:54:51 -0400

    Create assets.js

---
+++ content/assets.js
@@ -0,0 +1,34 @@
+const assets = `
+
+<!-- rubo yogurt -->
+<div class="section">
+    <h1>yogurt</h1>
+    <div>
+        <img src="assets_download/rubo.png" width="100%">
+
+        <p>a cute little vegan yogurt cup by the fictitious brand RUBO™.</p>
+        
+        <i class="fa-solid fa-shapes"></i> 132 polys
+        <br><i class="fa-solid fa-floppy-disk"></i> 479KB
+        <br><i class="fa-solid fa-image"></i> 1024×1024p
+        <br>
+        <a href="javascript:window.open('assets_download/rubo.zip','_blank');">download</a>
+    </div>
+</div>
+
+<!-- snub pistol -->
+<div class="section">
+    <h1>snub pistol</h1>
+    <div>
+        <img src="assets_download/snub_pistol.png" width="100%">
+
+        <p>a simple pistol model, with animatable slide, trigger, and mag. not textured.</p>
+        
+        <i class="fa-solid fa-shapes"></i> 367 polys
+        <br><i class="fa-solid fa-floppy-disk"></i> 946KB
+        <br>
+        <a href="javascript:window.open('assets_download/snub_pistol.zip','_blank');">download</a>
+    </div>
+</div>
+
+`;

67e973ab7dd9f32013964eb0bd09a6c9fa60ebaa
kitsumitsu <75032462+BlueBunnex@users.noreply.github.com>
2023-07-15 16:48:20 -0400

    yea this is good

--- characters.js
+++ characters.js
@@ -5,71 +5,71 @@ const characterInfo = {
         imgs: [""],
         reference: null,
         facts: [],
-        body: "wave along tail that allows for transition to bottom color",
+        body: "blue eyes/inner mouth, wave along tail that allows for transition to bottom color",
         clothing: "",
         personality: "loving",
         colors: [],
         paragraphs: [
-            "She lives in the countryside, owns a small pickup, farms agales ([ə.gä.lɛɪz], a type of large berry-bearing bush), and tends to some chickens and goats. Within each DIY wire-mesh on her property is an curious animal; Beneath every mound of soil overturned is a seed racing to germinate. While this seems like an unrealistic romanticization of the countryside (which it is), she fortunately receives a pension from an unknown sender through the bank every month, so all is well.",
+            "She lives on the beachside, owns a small pickup, farms agales ([ə.gä.lɛɪz], a type of large berry-bearing bush that grows well in sand), and tends to some chickens, rabbits, and goats. Within each DIY wire-mesh on her property is an curious animal; Beneath every mound of soil overturned is a little seed. While this seems like an unrealistic romanticization of the countryside (which it is), she fortunately receives a pension from an unknown sender through the bank every month, so all is well.",
         ],
         table_data: {
             Species: "Shark",
-            Identity: "Female",
-            Orientation: "Straight",
+            Identity: "Woman",
+            Orientation: "?",
             Weight: "Over",
             Height: "Very short"
         }
     },
-    'q': {
-        name: "q",
-        tagline: "cyber assistant",
-        imgs: ["qtie.png"],
-        reference: null,
-        facts: [
-            "They live in a computer, though they're 64-bit exclusive. They find Macbooks the coziest."
-        ],
-        body: "Pear-shaped.",
-        clothing: "Blue oven mitts, apron, and striped knee socks.",
-        personality: "Rampantly egotistical and annoying.",
-        colors: [
-            "#ffffff",
-            "#73d7ff"
-        ],
-        paragraphs: [],
-        table_data: {
-            Species: "x64",
-            Identity: "Non-binary (hexadecimal)",
-            Orientation: "N/A",
-            Weight: "4KB",
-            Height: "1024px"
-        }
-    },
-    'bonnie': {
-        name: "Bonnie",
-        tagline: "mercurial beachgoer",
-        imgs: ["bonnie_ex.png"],
-        reference: null,
-        facts: [
-            "She's married, though we don't know to whom just yet."
-        ],
-        body: "Pink hair, blue pupils/inner mouth. She has a muffintop while wearing tight clothes.",
-        clothing: "I'm still workshopping what her 'default' clothing will be. I'm thinking about having her wear a pink one piece underneath a white overcoat (with a single button connected at her midriff), and maybe a white sunhat with a blue bow and black shades. And of course, her golden bangles and heart necklace.",
-        personality: "Mercurial. Upbeat sometimes, and sleepy other times. Sometime's she's extremely talkative, other times she just wants to take a nap.",
-        colors: [
-            "#3639ff",
-            "#ff96c4"
-        ],
-        paragraphs: [
-            "Bonnie likes sunny days and rainy days. Looking out at the beach from her balcony, whether rain or shine, she is contented."
-        ],
-        table_data: {
-            Species: "Human",
-            Identity: "Woman",
-            Orientation: "Asexual, Romantic",
-            Weight: "Over",
-            Height: "Tall"
-        }
-    },
+    // 'q': {
+    //     name: "q",
+    //     tagline: "cyber assistant",
+    //     imgs: ["qtie.png"],
+    //     reference: null,
+    //     facts: [
+    //         "They live in a computer, though they're 64-bit exclusive. They find Macbooks the coziest."
+    //     ],
+    //     body: "Pear-shaped.",
+    //     clothing: "Blue oven mitts, apron, and striped knee socks.",
+    //     personality: "Rampantly egotistical and annoying.",
+    //     colors: [
+    //         "#ffffff",
+    //         "#73d7ff"
+    //     ],
+    //     paragraphs: [],
+    //     table_data: {
+    //         Species: "x64",
+    //         Identity: "Non-binary (hexadecimal)",
+    //         Orientation: "N/A",
+    //         Weight: "4KB",
+    //         Height: "1024px"
+    //     }
+    // },
+    // 'bonnie': {
+    //     name: "Bonnie",
+    //     tagline: "mercurial beachgoer",
+    //     imgs: ["bonnie_ex.png"],
+    //     reference: null,
+    //     facts: [
+    //         "She's married, though we don't know to whom just yet."
+    //     ],
+    //     body: "Pink hair, blue pupils/inner mouth. She has a muffintop while wearing tight clothes.",
+    //     clothing: "I'm still workshopping what her 'default' clothing will be. I'm thinking about having her wear a pink one piece underneath a white overcoat (with a single button connected at her midriff), and maybe a white sunhat with a blue bow and black shades. And of course, her golden bangles and heart necklace.",
+    //     personality: "Mercurial. Upbeat sometimes, and sleepy other times. Sometime's she's extremely talkative, other times she just wants to take a nap.",
+    //     colors: [
+    //         "#3639ff",
+    //         "#ff96c4"
+    //     ],
+    //     paragraphs: [
+    //         "Bonnie likes sunny days and rainy days. Looking out at the beach from her balcony, whether rain or shine, she is contented."
+    //     ],
+    //     table_data: {
+    //         Species: "Human",
+    //         Identity: "Woman",
+    //         Orientation: "Asexual, Romantic",
+    //         Weight: "Over",
+    //         Height: "Tall"
+    //     }
+    // },
     'cacao': {
         name: "Cacao",
         tagline: "snuggly chocolate wolf",

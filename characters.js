3bd1754315d8dfede4943ba7806573670bc5b904
kitsumitsu <75032462+BlueBunnex@users.noreply.github.com>
2023-07-16 11:21:47 -0400

    make data more concise

--- characters.js
+++ characters.js
@@ -2,10 +2,6 @@ const characterInfo = {
     'wip': {
         name: "wip shark",
         imgs: [""],
-        body: "blue eyes/inner mouth, wave along tail that allows for transition to bottom color",
-        clothing: "",
-        personality: "loving",
-        colors: [],
         paragraphs: [
             "She lives on the beachside, owns a small pickup, farms agales ([ə.gä.lɛɪz], a type of large berry-bearing bush that grows well in sand), and tends to some chickens, rabbits, and goats. Within each DIY wire-mesh on her property is a curious animal; Beneath every mound of soil overturned is a little seed. While this seems like an unrealistic romanticization of the country (which it is), she fortunately receives a pension from an unknown sender through the bank every month, so all is well.",
         ],
@@ -14,71 +10,15 @@ const characterInfo = {
             Identity: "Woman",
             Orientation: "?",
             Weight: "Over",
-            Height: "Very short"
+            Height: "Very short",
+            Body: "Blue eyes/inner mouth, wave along tail that allows for transition to bottom color.",
+            Clothing: "",
+            Personality: "Loving."
         }
     },
-    // 'q': {
-    //     name: "q",
-    //     tagline: "cyber assistant",
-    //     imgs: ["qtie.png"],
-    //     reference: null,
-    //     facts: [
-    //         "They live in a computer, though they're 64-bit exclusive. They find Macbooks the coziest."
-    //     ],
-    //     body: "Pear-shaped.",
-    //     clothing: "Blue oven mitts, apron, and striped knee socks.",
-    //     personality: "Rampantly egotistical and annoying.",
-    //     colors: [
-    //         "#ffffff",
-    //         "#73d7ff"
-    //     ],
-    //     paragraphs: [],
-    //     table_data: {
-    //         Species: "x64",
-    //         Identity: "Non-binary (hexadecimal)",
-    //         Orientation: "N/A",
-    //         Weight: "4KB",
-    //         Height: "1024px"
-    //     }
-    // },
-    // 'bonnie': {
-    //     name: "Bonnie",
-    //     tagline: "mercurial beachgoer",
-    //     imgs: ["bonnie_ex.png"],
-    //     reference: null,
-    //     facts: [
-    //         "She's married, though we don't know to whom just yet."
-    //     ],
-    //     body: "Pink hair, blue pupils/inner mouth. She has a muffintop while wearing tight clothes.",
-    //     clothing: "I'm still workshopping what her 'default' clothing will be. I'm thinking about having her wear a pink one piece underneath a white overcoat (with a single button connected at her midriff), and maybe a white sunhat with a blue bow and black shades. And of course, her golden bangles and heart necklace.",
-    //     personality: "Mercurial. Upbeat sometimes, and sleepy other times. Sometime's she's extremely talkative, other times she just wants to take a nap.",
-    //     colors: [
-    //         "#3639ff",
-    //         "#ff96c4"
-    //     ],
-    //     paragraphs: [
-    //         "Bonnie likes sunny days and rainy days. Looking out at the beach from her balcony, whether rain or shine, she is contented."
-    //     ],
-    //     table_data: {
-    //         Species: "Human",
-    //         Identity: "Woman",
-    //         Orientation: "Asexual, Romantic",
-    //         Weight: "Over",
-    //         Height: "Tall"
-    //     }
-    // },
     'cacao': {
         name: "Cacao",
         imgs: ["cacao0.png", "cacao_book.png", "cacao_ref.png"],
-        body: "A brown wolf that has a wide body type (but isn't overweight). She has purple-yellow heterochromia (purple: her right, yellow: her left), and her eye shine forms little hearts. Her hands, feet, and tail feature a bubble pattern as they transition from brown to tan. Her hands are human-like and four fingered, while her legs/feet are anthro.",
-        clothing: "Typically a red hoodie over a tan tee that reads ROFL across the front.",
-        personality: "Boisterous, well-intentioned, and colorful in nature, and seemingly always motivated to push herself to become better. She's kind, but push her around and she'll get ticked off fast.",
-        colors: [
-            "#eb4034",
-            "#fae3af",
-            "#d49b7b",
-            "#9c7767"
-        ],
         paragraphs: [
             "Cacao does not pull punches. She insists on ordering first at restaurants. She must be the one to hold the door open for the other person. And she absolutely is the one who picks out this week's groceries' chip flavour. Why does she do this? In her words, \"Some of us like to be getting stuff done!\"",
             "When she has trouble sleeping, she likes a little midnight hot chocolate. Her favourite food is a fried chicken sandwich with pickles and mayo. Also, she's snuggly."
@@ -88,7 +28,61 @@ const characterInfo = {
             Identity: "Woman",
             Orientation: "Straight",
             Weight: "Average",
-            Height: "Short"
+            Height: "Short",
+            Body: "A brown wolf that has a wide body type (but isn't overweight). She has purple-yellow heterochromia (purple: her right, yellow: her left), and her eye shine forms little hearts. Her hands, feet, and tail feature a bubble pattern as they transition from brown to tan. Her hands are human-like and four fingered, while her legs/feet are anthro.",
+            Clothing: "Typically a red hoodie over a tan tee that reads ROFL across the front.",
+            Personality: "Boisterous, well-intentioned, and colorful in nature, and seemingly always motivated to push herself to become better. She's kind, but push her around and she'll get ticked off fast."
         }
     }
 };
+
+// 'q': {
+//     name: "q",
+//     tagline: "cyber assistant",
+//     imgs: ["qtie.png"],
+//     reference: null,
+//     facts: [
+//         "They live in a computer, though they're 64-bit exclusive. They find Macbooks the coziest."
+//     ],
+//     body: "Pear-shaped.",
+//     clothing: "Blue oven mitts, apron, and striped knee socks.",
+//     personality: "Rampantly egotistical and annoying.",
+//     colors: [
+//         "#ffffff",
+//         "#73d7ff"
+//     ],
+//     paragraphs: [],
+//     table_data: {
+//         Species: "x64",
+//         Identity: "Non-binary (hexadecimal)",
+//         Orientation: "N/A",
+//         Weight: "4KB",
+//         Height: "1024px"
+//     }
+// },
+// 'bonnie': {
+//     name: "Bonnie",
+//     tagline: "mercurial beachgoer",
+//     imgs: ["bonnie_ex.png"],
+//     reference: null,
+//     facts: [
+//         "She's married, though we don't know to whom just yet."
+//     ],
+//     body: "Pink hair, blue pupils/inner mouth. She has a muffintop while wearing tight clothes.",
+//     clothing: "I'm still workshopping what her 'default' clothing will be. I'm thinking about having her wear a pink one piece underneath a white overcoat (with a single button connected at her midriff), and maybe a white sunhat with a blue bow and black shades. And of course, her golden bangles and heart necklace.",
+//     personality: "Mercurial. Upbeat sometimes, and sleepy other times. Sometime's she's extremely talkative, other times she just wants to take a nap.",
+//     colors: [
+//         "#3639ff",
+//         "#ff96c4"
+//     ],
+//     paragraphs: [
+//         "Bonnie likes sunny days and rainy days. Looking out at the beach from her balcony, whether rain or shine, she is contented."
+//     ],
+//     table_data: {
+//         Species: "Human",
+//         Identity: "Woman",
+//         Orientation: "Asexual, Romantic",
+//         Weight: "Over",
+//         Height: "Tall"
+//     }
+// },

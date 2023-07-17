10ecde25d62f43af75d42efbec4dbbac4688e9d5
kitsumitsu <75032462+BlueBunnex@users.noreply.github.com>
2023-07-17 18:11:13 -0400

    give stella name, reorganize table

--- content/characters.js
+++ content/characters.js
@@ -1,10 +1,7 @@
 const characterInfo = {
-    'wip': {
-        name: "wip shark",
+    'stella': {
+        name: "Stella Oei",
         imgs: ["stella_shark.png"],
-        paragraphs: [
-            "She lives on her space farm. Though she seems innocent, she's also a kleptomaniac, and steals donuts."
-        ],
         table_data: {
             Species: "Thresher Shark + Dragon",
             Identity: "Woman",
@@ -14,15 +11,15 @@ const characterInfo = {
             Body: "Pink body, blue eyes/inner mouth, wave along tail that allows for transition to bottom color.",
             Clothing: "I dunno yet.",
             Personality: "Loving."
-        }
+        },
+        paragraphs: [
+            "She lives on her space farm. Though she seems innocent, she's also a kleptomaniac, and steals donuts."
+            "Her last name is pronounced \"Wei\" [wɛɪ]."
+        ]
     },
     'cacao': {
         name: "Cacao",
         imgs: ["cacao0.png", "cacao_book.png", "cacao_ref.png"],
-        paragraphs: [
-            "Cacao does not pull punches. She insists on ordering first at restaurants. She must be the one to hold the door open for the other person. And she absolutely is the one who picks out this week's groceries' chip flavour. Why does she do this? In her words, \"Some of us like to be getting stuff done!\"",
-            "When she has trouble sleeping, she likes a little midnight hot chocolate. Her favourite food is a fried chicken sandwich with pickles and mayo. Also, she's snuggly."
-        ],
         table_data: {
             Species: "Wolf",
             Identity: "Woman",
@@ -32,7 +29,11 @@ const characterInfo = {
             Body: "A brown wolf that has a wide body type (but isn't overweight). She has purple-yellow heterochromia (purple: her right, yellow: her left), and her eye shine forms little hearts. Her hands, feet, and tail feature a bubble pattern as they transition from brown to tan. Her hands are human-like and four fingered, while her legs/feet are anthro.",
             Clothing: "Typically a red hoodie over a tan tee that reads ROFL across the front.",
             Personality: "Boisterous, well-intentioned, and colorful in nature, and seemingly always motivated to push herself to become better. She's kind, but push her around and she'll get ticked off fast."
-        }
+        },
+        paragraphs: [
+            "Cacao does not pull punches. She insists on ordering first at restaurants. She must be the one to hold the door open for the other person. And she absolutely is the one who picks out this week's groceries' chip flavour. Why does she do this? In her words, \"Some of us like to be getting stuff done!\"",
+            "When she has trouble sleeping, she likes a little midnight hot chocolate. Her favourite food is a fried chicken sandwich with pickles and mayo. Also, she's snuggly."
+        ]
     }
 };
 

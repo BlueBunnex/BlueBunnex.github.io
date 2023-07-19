bfc5ccba32598068cec71e3693c58e1b566cee9b
kitsumitsu <75032462+BlueBunnex@users.noreply.github.com>
2023-07-19 14:20:37 -0400

    add shirt stuff

--- content/assets.js
+++ content/assets.js
@@ -4,15 +4,15 @@ const assets = `
 <div class="section">
     <h1>yogurt</h1>
     <div>
-        <img src="assets_download/rubo.png" style="width: 30%; cursor: zoom-in;" onclick="window.open('assets_download/rubo.png');">
+        <img src="assets_download/rubo.png" style="width: 30%; cursor: zoom-in;" onclick="window.open('assets_download/rubo.png','_blank');">
 
         <p>a cute little vegan yogurt cup by the fictitious brand RUBO™.</p>
-        
-        <i class="fa-solid fa-shapes"></i> 132 polys
-        <br><i class="fa-solid fa-floppy-disk"></i> 479KB
-        <br><i class="fa-solid fa-image"></i> 1024×1024p
-        <br>
-        <a href="javascript:window.open('assets_download/rubo.zip','_blank');">download</a>
+
+        <ul>
+            <li>132 polys</li>
+            <li>479KB</li>
+            <li>1024×1024p</li>
+        </ul>
     </div>
 </div>
 
@@ -20,14 +20,29 @@ const assets = `
 <div class="section">
     <h1>snub pistol</h1>
     <div>
-        <img src="assets_download/snub_pistol.png" style="width: 30%; cursor: zoom-in;" onclick="window.open('assets_download/snub_pistol.png');">
+        <img src="assets_download/snub_pistol.png" style="width: 30%; cursor: zoom-in;" onclick="window.open('assets_download/snub_pistol.png','_blank');">
 
         <p>a simple pistol model, with animatable slide, trigger, and mag. not textured.</p>
-        
-        <i class="fa-solid fa-shapes"></i> 367 polys
-        <br><i class="fa-solid fa-floppy-disk"></i> 946KB
-        <br>
-        <a href="javascript:window.open('assets_download/snub_pistol.zip','_blank');">download</a>
+
+        <ul>
+            <li>367 polys</li>
+            <li>946KB</li>
+        </ul>
+    </div>
+</div>
+
+<!-- funny shirt textures -->
+<div class="section">
+    <h1>fish to catch shirt transparents</h1>
+    <div>
+        <img src="assets_download/of_course_shirt.png" style="width: 30%; cursor: zoom-in;" onclick="window.open('assets_download/of_course_shirt.png','_blank');">
+        <img src="assets_download/fish_to_catch_shirt.png" style="width: 30%; cursor: zoom-in;" onclick="window.open('assets_download/fish_to_catch_shirt.png','_blank');">
+
+        <p>some textures you could superimpose onto a VRChat avatar's blank shirt texture (or for something else, if you're feeling adventurous).</p>
+
+        <ul>
+            <li>1600×1600p</li>
+        </ul>
     </div>
 </div>
 

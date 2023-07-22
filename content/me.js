46ce25cc9d7a6aa44f408e0871cf65c0dcdee9cc
kitsumitsu <75032462+BlueBunnex@users.noreply.github.com>
2023-07-22 09:41:18 -0400

    likes wearing

--- content/me.js
+++ content/me.js
@@ -4,11 +4,18 @@ const me = `
     <h1>fursona</h1>
     <div>
         <img src='img/kobold.png'>
+        
+        <h1>general</h1>
         <ul>
             <li>male kobold (he/him), a little fat</li>
             <li>is the quiet one of the group, but only because his passion only emerges when speaking one-on-one</li>
             <li>likes sitting on the floor next to others</li>
             <li>the interior of their mouth is also blue like their eyes and horns</li>
+        </ul>
+
+        <h1>likes wearing</h1>
+        <ul>
+            <li>idk</li>
         </ul>
     </div>
 </div>

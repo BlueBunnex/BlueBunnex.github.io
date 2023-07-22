ed77c515bfce970cb516347c9a1f0934a5f7e83e
kitsumitsu <75032462+BlueBunnex@users.noreply.github.com>
2023-07-22 15:47:47 -0400

    optimize

--- content/me.js
+++ content/me.js
@@ -1,14 +1,14 @@
 const me = `
 
 <div class="section">
-    <h1>fursona</h1>
+    <h1>my fursona</h1>
     <div>
         <img src='img/kobold.png'>
         
         <h1>general</h1>
         <ul>
             <li>male kobold (he/him), a little fat</li>
-            <li>character art is still WIP!</li>
+            <li>character art is still WIP! actually a lot of things are still WIP >w<</li>
             <li>the quiet one of the group, but only because his passion only emerges when speaking one-on-one</li>
             <li>likes sitting on the floor next to others</li>
             <li>the interior of their mouth is also blue like their eyes and horns</li>
@@ -19,9 +19,9 @@ const me = `
             <li>idk</li>
         </ul>
 
-        <h1>3D models</h1>
-        <img src='img/kobold64_0.png' height="100">
-        <img src='img/kobold64_1.png' height="100">
+        <h1>3D model</h1>
+        <img src='img/kobold64_0.png' height="160">
+        <img src='img/kobold64_1.png' height="160">
     </div>
 </div>
 

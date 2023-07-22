ae4f63ba37500f9b911fcec8af6e0d91194f66d0
kitsumitsu <75032462+BlueBunnex@users.noreply.github.com>
2023-07-21 21:37:57 -0400

    text

--- content/me.js
+++ content/me.js
@@ -1,9 +1,10 @@
 const me = `
 
 <div class="section">
-    <h1>kobold fursona</h1>
+    <h1>kobold fursona maybe idk</h1>
     <div>
         <img src='img/kobold.png'>
+        male, likes speaking passionately and also hugs
     </div>
 </div>
 

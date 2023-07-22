5b848f847d17dbfa6e6283376ae372197f42b6a4
kitsumitsu <75032462+BlueBunnex@users.noreply.github.com>
2023-07-21 22:55:55 -0400

    blue

--- content/me.js
+++ content/me.js
@@ -1,13 +1,14 @@
 const me = `
 
 <div class="section">
-    <h1>kobold fursona maybe idk</h1>
+    <h1>fursona</h1>
     <div>
         <img src='img/kobold.png'>
         <ul>
             <li>male kobold (he/him), a little fat</li>
             <li>is the quiet one of the group, but only because his passion only emerges when speaking one-on-one</li>
             <li>likes sitting on the floor next to others</li>
+            <li>the interior of their mouth is also blue like their eyes and horns</li>
         </ul>
     </div>
 </div>

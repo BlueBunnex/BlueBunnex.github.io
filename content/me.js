a1078e19762f3fa0459102b43f86d17870ead3e6
kitsumitsu <75032462+BlueBunnex@users.noreply.github.com>
2023-07-21 22:35:09 -0400

    male pronouns

--- content/me.js
+++ content/me.js
@@ -5,8 +5,8 @@ const me = `
     <div>
         <img src='img/kobold.png'>
         <ul>
-            <li>male</li>
-            <li>is the quiet one of the group, but only because their passion only emerges when speaking one-on-one</li>
+            <li>male kobold (he/him), a little fat</li>
+            <li>is the quiet one of the group, but only because his passion only emerges when speaking one-on-one</li>
             <li>likes sitting on the floor next to others</li>
         </ul>
     </div>

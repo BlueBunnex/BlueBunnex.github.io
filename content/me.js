219b562589cade7b80db55e38b8e7c222394b790
kitsumitsu <75032462+BlueBunnex@users.noreply.github.com>
2023-07-23 15:51:24 -0400

    box

--- content/me.js
+++ content/me.js
@@ -3,11 +3,12 @@ const me = `
 <div class="section">
     <h1>my fursona</h1>
     <div>
-        <p>idk what my fursona is gonna be (maybe a dragon? because chonky tail), but here's some of the descriptions that I plan on including, because they are details that I like about myself.</p>
+        <p>idk what my fursona is gonna be (maybe a fluffy dragon? because chonky tail), but here's some of the descriptions that I plan on including, because they are details that I like about myself.</p>
 
         <h1>general</h1>
         <ul>
            <li>Boisterous sometimes, sleepy other times.</li>
+           <li>Likes boxes and sitting in boxes.</li>
         </ul>
 
         <h1>likes wearing</h1>

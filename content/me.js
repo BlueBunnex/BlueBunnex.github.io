d897c7a834ac5959790e2784e26f22d49c49bab5
kitsumitsu <75032462+BlueBunnex@users.noreply.github.com>
2023-07-22 23:09:42 -0400

    wrap

--- content/me.js
+++ content/me.js
@@ -3,13 +3,13 @@ const me = `
 <div class="section">
     <h1>my fursona</h1>
     <div>
-        I don't know, honestly. I'll come up with my representation of me when I get there
+        <p>I don't know, honestly. I'll come up with my representation of me when I get there. nevertheless, here's some of the descriptions that I plan on including, because they are details that I like about myself.</p>
+
+        <h1>general</h1>
+        <ul>
+           <li>Boisterous sometimes, sleepy other times.</li>    
+        </ul>
     </div>
 </div>
 
 `;
-
-//<h1>general</h1>
-//<ul>
-//    <li>Boisterous sometimes, sleepy other times.</li>    
-//</ul>

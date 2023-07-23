da6e73147324392115c6cfe7f2c69b438db66a8f
kitsumitsu <75032462+BlueBunnex@users.noreply.github.com>
2023-07-23 15:22:22 -0400

    update

--- content/me.js
+++ content/me.js
@@ -3,16 +3,16 @@ const me = `
 <div class="section">
     <h1>my fursona</h1>
     <div>
-        <p>idk what my fursona is gonna be. nevertheless, here's some of the descriptions that I plan on including, because they are details that I like about myself.</p>
+        <p>idk what my fursona is gonna be, but here's some of the descriptions that I plan on including, because they are details that I like about myself.</p>
 
         <h1>general</h1>
         <ul>
-           <li>Boisterous sometimes, sleepy other times.</li>    
+           <li>Boisterous sometimes, sleepy other times.</li>
         </ul>
 
         <h1>likes wearing</h1>
         <ul>
-           <li>Knee-socks.</li>    
+           <li>Knee-socks.</li>
         </ul>
     </div>
 </div>

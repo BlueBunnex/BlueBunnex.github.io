dd067f5520103023b96e22432f0779a102cb1138
kitsumitsu <75032462+BlueBunnex@users.noreply.github.com>
2023-07-22 22:20:45 -0400

    truth.ffmpeg

--- content/me.js
+++ content/me.js
@@ -3,22 +3,7 @@ const me = `
 <div class="section">
     <h1>my fursona</h1>
     <div>
-        <img src='img/kobold.png'>
-        
-        <h1>general</h1>
-        <ul>
-            <li>male kobold (he/him), a little fat</li>
-            <li>character art is still WIP! actually a lot of things are still WIP >w<</li>
-            <li>energetic half the time, and sleepy the other half</li>
-            <li>the quiet one of the group, but only because his passion only emerges when speaking one-on-one</li>
-            <li>likes sitting on the floor next to others</li>
-            <li>the interior of their mouth is also blue like their eyes and horns</li>
-        </ul>
-
-        <h1>likes wearing</h1>
-        <ul>
-            <li>idk</li>
-        </ul>
+        I don't know, honestly. I'll come up with my representation of me when I get there
     </div>
 </div>
 

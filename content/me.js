4c03cb35828e13adb9d1af5e3356a574ca55f36f
kitsumitsu <75032462+BlueBunnex@users.noreply.github.com>
2023-07-24 22:12:09 -0400

    omg

--- content/me.js
+++ content/me.js
@@ -17,10 +17,10 @@ const me = `
         </ul>
 
         <span>copy: </span>
-        <a href="javascript:navigator.clipboard.writeText('#dd898c');" style="color: #dd898c; background: black; font-weight: 1000;">#dd898c </a>
-        <a href="javascript:navigator.clipboard.writeText('#e7bab1');" style="color: #e7bab1; background: black; font-weight: 1000;">#e7bab1 </a>
-        <a href="javascript:navigator.clipboard.writeText('#ffeac5');" style="color: #ffeac5; background: black; font-weight: 1000;">#ffeac5 </a>
-        <a href="javascript:navigator.clipboard.writeText('#fff9e9');" style="color: #fff9e9; background: black; font-weight: 1000;">#fff9e9 </a>
+        <a href="javascript:navigator.clipboard.writeText('#dd898c');" style="background: #dd898c;">omg</a>
+        <a href="javascript:navigator.clipboard.writeText('#e7bab1');" style="background: #e7bab1;">omg</a>
+        <a href="javascript:navigator.clipboard.writeText('#ffeac5');" style="background: #ffeac5;">omg</a>
+        <a href="javascript:navigator.clipboard.writeText('#fff9e9');" style="background: #fff9e9;">omg</a>
 
         <img src="img/fursona_yes.png" alt="My fursona, a bipedal, yellow and red plush drake with heart tattoos.">
     </div>

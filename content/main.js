b25fcec8183534e3e540ceadbe2a2c981f427c2e
kitsumitsu <75032462+BlueBunnex@users.noreply.github.com>
2023-07-25 22:18:43 -0400

    no more universe

--- content/main.js
+++ content/main.js
@@ -8,20 +8,9 @@ const main = `
                 </div>
             </div>
 
-            <!--
-            <div class="section">
-                <h1>the kitsumitsuverse (ocs)</h1>
-                <h2>#001</h2>
-                <div>
-                    <p>the kitsumitsuverse is a collection of characters that I like to draw! all my favourite ocs are featured here. all my characters are adults.</p>
-                    <a class="ref" href="javascript:loadContent(universe);"></a>
-                </div>
-            </div>
-            -->
-
             <div class="section">
                 <h1>asset downloads</h1>
-                <h2>#002</h2>
+                <h2>#001</h2>
                 <div>
                     <p>some assets that you can use if you like, mostly 3D stuff. they're all free and you don't have to give credit, but you know, you can!</p>
                     <a class="ref" href="javascript:loadContent(assets);"></a>
@@ -30,7 +19,7 @@ const main = `
 
             <div class="section">
                 <h1>my conlanging project(s)</h1>
-                <h2>#003</h2>
+                <h2>#002</h2>
                 <div>
                     <p>I like to make these constructed languages. however, I'm very indecisive and often restart over and over trying to get something I like. while that's happening, you can see what I'm currently working on in that regard here.</p>
                     <a class="ref" href="javascript:loadContent(conlanging);"></a>
@@ -39,7 +28,7 @@ const main = `
 
             <div class="section">
                 <h1>my conspecies: the limalh</h1>
-                <h2>#004</h2>
+                <h2>#003</h2>
                 <div>
                     <p>I mocked up a design for a fictional species of humanoid creatures called the limalh. I plan on drawing various features for the limalh that can be used to create variations in their designs (maybe, if I get around to it). if you want to make a limalh oc, feel free! and if you could share it with me on discord or something, that would be cool!</p>
                     <a class="ref" href="javascript:loadContent(conspecies);"></a>

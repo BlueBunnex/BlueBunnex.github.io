417f66b1e9e572370c8b0347040c3e1eff8c39ba
kitsumitsu <75032462+BlueBunnex@users.noreply.github.com>
2023-07-26 13:51:37 -0400

    add species

--- content/conlanging.js
+++ content/conlanging.js
@@ -47,3 +47,23 @@ const cabuLanguage = `
 </div>
 
 `;
+
+const limalhSpecies = `
+
+<div class="section">
+    <h1>Limalh</h1>
+    <div>
+        <img src="img/limalh_thing.png">
+        <br>
+        Limalh [li.maʎ] are native to the _ region of _. They come with lots of variants at varying rarities that I will come up with later (very cool).
+        <br>
+        <br>
+        tail varieties<br>
+        <strong style="color: white; background: green;">common</strong> bulb, long<br>
+        <strong style="color: white; background: blue;">uncommon</strong> split end, spade<br>
+        <strong style="color: white; background: purple;">rare</strong> double, loop<br>
+        <strong style="color: white; background: red;">very rare</strong> ??
+    </div>
+</div>
+
+`;

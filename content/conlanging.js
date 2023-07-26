3751dde5ff604abb2d88018d43bf8e92211e51c4
kitsumitsu <75032462+BlueBunnex@users.noreply.github.com>
2023-07-25 22:59:58 -0400

    how will caps work

--- content/conlanging.js
+++ content/conlanging.js
@@ -1,64 +1,15 @@
-const conlanging = `
+const cabuLanguage = `
 
 <div class="section">
-    <h1>unnamed conlang ig</h1>
+    <h1>Cabü Overview</h1>
     <div>
         <p>Eventually I'll have it set up in a way so that it is ordered to be easily learned step by step, but w/e</p>
-        
-        <h3>Phonology</h3>
-        <table style="width: 100%; text-align: center;">
-            <tbody>
-                <tr>
-                    <th></th>
-                    <th>Labial</th>
-                    <th>Alveolar</th>
-                    <th>Palatal</th>
-                    <th>Velar</th>
-                    <th>Glottal</th>
-                </tr>
-                <tr>
-                    <th>Nasal</th>
-                    <td>m <i>m</i></td>
-                    <td>n <i>n</i></td>
-                    <td></td>
-                    <td>ŋ <i>ng</i></td>
-                    <td></td>
-                </tr>
-                <tr>
-                    <th>Plosive</th>
-                    <td>p <i>p</i></td>
-                    <td>t <i>t</i></td>
-                    <td></td>
-                    <td>k <i>k</i></td>
-                    <td>ʔ <i>q</i></td>
-                </tr>
-                <tr>
-                    <th>Affricate</th>
-                    <td></td>
-                    <td>ts <i>c</i></td>
-                    <td>tsʲ~tɕ <i>ch</i></td>
-                    <td></td>
-                    <td></td>
-                </tr>
-                <tr>
-                    <th>Fricative</th>
-                    <td></td>
-                    <td>s <i>s</i></td>
-                    <td>ɕ <i>sh</i></td>
-                    <td></td>
-                    <td>h <i>h</i></td>
-                </tr>
-                <tr>
-                    <th>Liquid</th>
-                    <td>w <i>w</i></td>
-                    <td></td>
-                    <td>j <i>j</i></td>
-                    <td></td>
-                    <td></td>
-                </tr>
-            </tbody>
-        </table>
-        
+    </div>
+</div>
+
+<div class="section">
+    <h1>Phonology</h1>
+    <div>
         <table style="width: 50%; text-align: center;">
             <tbody>
                 <tr>
@@ -87,59 +38,6 @@ const conlanging = `
                 </tr>
             </tbody>
         </table>
-        
-        <ol>
-            <h3>Notes</h3>
-            <li>CVC</li>
-            <li>/j/, /s/ and /ts/ cannot occur before /i/ or /ə/.</li>
-            <li>/w/ cannot occur before /u/.</li>
-            <li>/ʔ/ only occurs medially.</li>
-        </ol>
-        
-        <hr>
-
-        copula drop yaay
-            
-        <h3>Lexicon</h3>
-        <ul>
-            <h3>Pronouns</h3>
-            <li>ange / 2nd person singular</li>
-            <li>angange / 2nd person plural</li>
-        </ul>
-        
-        <ul>
-            <h3>Nouns</h3>
-            <li>juche / person, human-being</li>
-            <li>paqan / room</li>
-        </ul>
-        
-        <ul>
-            <h3>Postpositions</h3>
-            <li>ep / follows the direct object (Accusative)</li>
-            <li>te / indicates the location/time where the action takes place (Locative)</li>
-        </ul>
-        
-        <ul>
-            <h3>Verbs</h3>
-            <li>chit / </li>
-        </ul>
-        
-        <ul>
-            <h3>Finals</h3>
-            <li>eng / Interrogative</li>
-            <li>cau / Optative</li>
-        </ul>
-        
-        <hr>
-        
-        <h3>Reference Grammar</h3>
-        head-final, SOV
-        
-        <hr>
-        
-        Juche paqan te Ø cau.<br>
-        gloss. S O pos V final<br>
-        trans. I hope the person is in the room.
     </div>
 </div>
 

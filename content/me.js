1ff75613f6c9535a2810859c0ba68da0c34e948d
kitsumitsu <75032462+BlueBunnex@users.noreply.github.com>
2023-07-25 23:15:04 -0400

    update wording, add some stuff

--- content/me.js
+++ content/me.js
@@ -5,9 +5,9 @@ const me = `
     <div>
         <h1>general</h1>
         <ul>
-            <li>A male (he/him) red-white plush drake (aka wingless dragon) with a chonky tail who is a little fat.</li>
+            <li>A male (he/him) red-white plush drake (aka wingless dragon) with a chonky tail who is a little fat. He is magical, and when not out and about he returns to his home inside a pocket dimension the size of a small apartment.</li>
             <li>Boisterous sometimes, sleepy other times. So he can muster up the energy to lead a whole group, but not after long he'd begin to wish for an opportunity for someone else to just speak at him while he lies quiety beside them.</li>
-            <li>While not explicitly a part of any fictional universe I create (being me myself), he has knowledge of all of them, including partial fluency of the conlangs they contain.</li>
+            <li>While not explicitly a part of any fictional universe I create (being myself), he has knowledge of all of them, including partial fluency of the conlangs they contain.</li>
         </ul>
 
         <h1>likes wearing</h1>
@@ -20,7 +20,7 @@ const me = `
         <h1>lore</h1>
         <ul>
            <li>Works part-time at Café de Andréa. His favourite drink is the iced latté, and his favourite menu item is the key-lime pie.</li>
-           <li>Wasn't always plush (the backstory for this is TBD).</li>
+           <li>Wasn't always plush (the backstory for this is TBD, but will include something about being dragged into a magical rift).</li>
         </ul>
 
         <a href="javascript:navigator.clipboard.writeText('#dd898c');" style="background: #dd898c;">copy</a>

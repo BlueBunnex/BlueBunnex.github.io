3821d8f8e36f8d5a8c862dac2b63f85a3ffec59f
kitsumitsu <75032462+BlueBunnex@users.noreply.github.com>
2023-07-26 22:23:13 -0400

    one para yaay

--- content/freewrite.js
+++ content/freewrite.js
@@ -3,7 +3,11 @@ const extras = null;
 const chapters = [
 `
 <p>
-Super cool intro hook
+So, problem. My eyes have stopped working and I'm letting it get to me. Okay, I'm really letting it get to me, I'm stood in the middle of my cell and I'm holding my arms
+out in front of me and, no, nothing there, and I slowly swing them to either side and, yup, nothing there either. The bed must be, uhm, <i>somewhere</i> around here, I don't
+remember which direction I was looking when everything suddenly stopped being visible.
+</p>
+<p>
 </p>
 `
 ];

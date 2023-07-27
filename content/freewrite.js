6590ebcbdb687626046c85de06787dc1fe6d6172
kitsumitsu <75032462+BlueBunnex@users.noreply.github.com>
2023-07-26 22:56:40 -0400

    blindfold

--- content/freewrite.js
+++ content/freewrite.js
@@ -3,17 +3,15 @@ const extras = null;
 const chapters = [
 `
 <p>
-So, problem. My eyes have stopped working and I'm letting it get to me. Okay, I'm really letting it get to me, I'm stood in the middle of the living room and I'm holding my arms
-out in front of me and, no, nothing there, and I slowly swing them to either side and, yup, nothing there either. The couch must be, uhm, <i>somewhere</i> around here, I don't
-remember which direction I was looking when everything suddenly stopped being visible.
+So, problem. My eyes have stopped working and I'm letting it get to me. Okay, I'm really letting it get to me, I'm stood in the middle of the living room and I'm holding my arms out in front of me and, no, nothing there, and I slowly swing them to either side and, yup, nothing there either. The couch must be, uhm, <i>somewhere</i> around here, I don't remember which direction I was looking when everything suddenly stopped being visible.
 </p><p>
 Right, so it's kind of scary just standing in what is now practically a jungle gym of stuff I can trip over, so I'm going to just kneel down and--
 </p><p>
 <i>Where'd the carpet go. Why am I standing on sand.</i>
 </p><p>
-As a momentary spark in my brain tells me how screwed I am my hand sinks sideways through the sand and I am caught by a bed of the--surprisingly soft--sand. <i>Why is my apartment...
-sand?</i> No, this can't be my apartment. Somehow by some means, probably the same means that also made me unable to see, I have been transported... <i>somewhere.</i>
+As a momentary spark in my brain tells me how fucked I am my hand sinks sideways through the sand and I am caught by a bed of the--surprisingly soft--sand. <i>Why is my apartment... sand?</i> No, this can't be my apartment. Somehow by some means, probably the same means that also made me unable to see, I have been transported... <i>somewhere.</i>
+</p><p>
+I push myself up from the sand, which is done with some difficulty due to how slippery it is. It isn't course at all, actually, more like flour than sand, but flour doesn't typically make up the ground. I rub my fingers together and of course they're dry and somewhat slick with the floursand, so I swipe my hands against my shirt which billows off even more of the stuff, because of course it does, and I pat my face to get off the sand there and <strong>I'm wearing a fucking blindfold.</strong>
 </p>
 `
 ];
-// there's actually something on their face

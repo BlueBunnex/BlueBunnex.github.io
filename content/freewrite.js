7a0e94cc91c2e8e93007e784bd38cece400840af
kitsumitsu <75032462+BlueBunnex@users.noreply.github.com>
2023-07-26 21:45:25 -0400

    intro hook

--- content/freewrite.js
+++ content/freewrite.js
@@ -2,6 +2,8 @@ const extras = null;
 
 const chapters = [
 `
-  Super cool intro hook for the very first chapter of this infinitely long story that will evolve over time.
+<p>
+Super cool intro hook
+</p>
 `
 ];

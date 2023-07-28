9bc2c177d2ef635ca64d4f9033fa7c83a72505a3
kitsumitsu <75032462+BlueBunnex@users.noreply.github.com>
2023-07-28 16:07:43 -0400

    dven

--- content/freewrite.js
+++ content/freewrite.js
@@ -11,7 +11,7 @@ Right, so it's kind of scary just standing in what is now practically a jungle g
 </p><p>
 As a momentary spark in my brain tells me how fucked I am my hand sinks sideways through the sand and I am caught by a bed of the--surprisingly soft--sand. <i>Why is my apartment... sand?</i> No, this can't be my apartment. Somehow by some means, probably the same means that also made me unable to see, I have been transported... <i>somewhere.</i>
 </p><p>
-I push myself up from the sand, which is done with some difficulty due to how slippery it is. It isn't course at all, actually, more like flour than sand, but flour doesn't typically make up the ground. I rub my fingers together and of course they're dry and somewhat slick with the floursand, so I swipe my hands against my shirt which billows off even more of the stuff, because of course it does, and I pat my face to get off the sand there and <strong>I'm wearing a fucking blindfold.</strong>
+I push myself up from the sand, which is done with some difficulty due to how slippery it is. It isn't course at all, actually, more like flour than sand, but flour doesn't typically make up the ground. I rub my fingers together and of course they're dry and somewhat slick with the floursand, so I swipe my hands against my shirt which billows off even more of the stuff, because of course it does, and I pat my face to get off the sand there and <i>I'm wearing a fucking blindfold.</i>
 </p><p>
 So that obviously comes off, and the whole world is pink for some reason, the sand, the sky, and I'm in the middle of jack-all nowhere, not a house or tree or rock in sight. It's just the sand. Everything is sand. Dunes and dunes and dunes and dunes. No--focus, Nile--look over there, in the distance. Mountains. Actually, in every direction, just a ring of moutains. Are they also made of sand? My feet have already sunk considerably into the floursand, and I realize just how badly that's unnerving me. What is going on. I start to trudge in the direction where the mountains seem the closest.
 </p><p>
@@ -34,13 +34,13 @@ As I pass into the city the density of people grows slightly, and I catch a few 
 
 // like dragons with dull, damaged, blueish-orange skin. Wait, blueish-orange? Blue and orange are on the opposite side of the color wheel, blueish orange is an impossible color--and yet that's what I see when I look at xer. I <i>feel</i> blue. But I also feel orange.
 
-// frustrated
 // VSO
 // verb inflect: -Ø = 3sg, -d = 2sg, -f = 1sg, -? = negative
 // nizi = they are cold
 // lani = they are called
 // ige = they say
 
+// "Aj! Dvenlani?" Xe raises xer hands as if getting ready to clap, then bunches the tips of her fingers together, then holds up her left pinky while pressing her other palm into her hand, then points at me. I just stare back at xer, and xe drops xer hands defeated, looking visibly frustrated that xe can't get an idea across, even in this second form of communication that I also don't recognize at all.
 // "What are you saying?" I say more to myself than to xer.
 // "<i>Wata ju sejin?</i> Kita?" xe mimicks. "Iged kita?"
 // I put it together in my head. "Kita?" I try to really emphasis the rising tone. Xe stares at me expectantly, I point to xer, repeat, "Kita?"

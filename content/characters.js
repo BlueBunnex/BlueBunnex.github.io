29ce82e18106c7a4247f1122b68cdb2500c89b20
kitsumitsu <75032462+BlueBunnex@users.noreply.github.com>
2023-07-29 22:55:32 -0400

    toy

--- content/characters.js
+++ content/characters.js
@@ -3,26 +3,9 @@ const characterInfo = {
         name: "Oli",
         imgs: ["oli.png"],
         table_data: {
-            Body: "Pudgy"
+            Body: "Pudgy, tall",
+            Character: "Snarky, playful"
         },
         paragraphs: []
-    },
-    'cacao': {
-        name: "Cacao Clifton",
-        imgs: ["cacao0.png", "cacao_book.png", "cacao_ref.png"],
-        table_data: {
-            Species: "Wolf",
-            Identity: "Woman",
-            Orientation: "Straight",
-            Weight: "Average",
-            Height: "Short",
-            Body: "A brown wolf that has a wide body type (but isn't overweight). She has purple-yellow heterochromia (purple: her right, yellow: her left), and her eye shine forms little hearts. Her hands, feet, and tail feature a bubble pattern as they transition from brown to tan. Her hands are human-like and four fingered, while her legs/feet are anthro.",
-            Clothing: "Typically a red hoodie over a tan tee that reads ROFL across the front.",
-            Personality: "Boisterous, well-intentioned, and colorful in nature, and seemingly always motivated to push herself to become better. She's kind, but push her around and she'll get ticked off fast."
-        },
-        paragraphs: [
-            "Cacao does not pull punches. She insists on ordering first at restaurants. She must be the one to hold the door open for the other person. And she absolutely is the one who picks out this week's groceries' chip flavour. Why does she do this? In her words, \"Some of us like to be getting stuff done!\"",
-            "When she has trouble sleeping, she likes a little midnight hot chocolate. Her favourite food is a fried chicken sandwich with pickles and mayo. Also, she's snuggly."
-        ]
     }
 };

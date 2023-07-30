6cf26af0c80e9a1880aca336bf9d827fcfc38ab8
kitsumitsu <75032462+BlueBunnex@users.noreply.github.com>
2023-07-29 22:48:08 -0400

    universe

--- content/main.js
+++ content/main.js
@@ -9,8 +9,17 @@ let construct = `
 </div>
 
 <div class="section">
-    <h1>conworlds, conlangs, conspecies, oh my!</h1>
+    <h1>the kitsumitsuverse</h1>
     <h2>#001</h2>
+    <div>
+        <p>aka my hot hot ocs</p>
+        <a class="ref" href="javascript:loadContent(universe);">check it out</a>
+    </div>
+</div>
+
+<div class="section">
+    <h1>conworlds, conlangs, conspecies, oh my!</h1>
+    <h2>#002</h2>
     <div>
         <p>I'm trying to build together a little fantasy world that features (at least) one conlang for me to work on, since I'm a linguist (who enjoys writing)! I'm thinking gothic sci-fi mecha fantasy.</p>
         <a class="ref" href="javascript:loadContent(cabuLanguage);">Cabü (conlang)</a>
@@ -20,7 +29,7 @@ let construct = `
 
 <div class="section">
     <h1>infinite freewrite</h1>
-    <h2>#002</h2>
+    <h2>#003</h2>
     <div>
         <p>I want to make something that I can just spend ten minutes working on every once in a while, so here it shall reside. essentially, it's gonna be one long story that evolves to suit whatever I feel like writing at the time. it might get messy, but I don't plan on taking it too seriously anywho.</p>
         <p>CW: swearing</p>
@@ -30,7 +39,7 @@ let construct = `
 
 <div class="section">
     <h1>asset downloads</h1>
-    <h2>#003</h2>
+    <h2>#004</h2>
     <div>
         <p>some assets that you can use if you like, mostly 3D stuff. they're all free and you don't have to give credit, but you know, you can!</p>
         <a class="ref" href="javascript:loadContent(assets);">check it out</a>

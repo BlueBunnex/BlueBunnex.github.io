b64bc55ed88337bd94058e2d546202993eaafdf3
kitsumitsu <75032462+BlueBunnex@users.noreply.github.com>
2023-07-29 22:45:54 -0400

    oli

--- content/characters.js
+++ content/characters.js
@@ -1,32 +1,11 @@
 const characterInfo = {
-    'rieler-county': {
-        name: "Rieler County",
-        imgs: [],
+    'oli': {
+        name: "Oli",
+        imgs: ["oli.png"],
         table_data: {
-            Population: "6242 people",
-            Astropotency: "34 nouls",
+            Body: "Pudgy"
         },
-        paragraphs: [
-            "Small places make for the biggest of communities, and nowhere is this more true than the humble Rieler County, home of the marshmallow-filled donut. With a population of just over six-thousand, it seems like everybody can recognize anyone else just by the color of their hair and the sound of their voice. Though small, Rieler County is urban and thus boasts rather impressive buildings, including their four-story 24/7 RGB shopping mall just next to the plaza where events are held. At night the streets are lit with the glow of giant screens on the mall advertising everything and their sister, and walking down the street with your friends, everyone talking with eachother, truly makes the plaza special to those residents who visit it time and time again.",
-            "With an astropotency pushing forty, Rieler County is known for some strange astronomical phenomena indeed, ranging from mild atmospheric abberation to some rather <i>unexpected</i> encounters."
-        ]
-    },
-    'van': {
-        name: "Van Daiser",
-        imgs: ["dog_guy.png"],
-        table_data: {
-            Species: "Beagle",
-            Identity: "Man",
-            Orientation: "Gay (closeted)",
-            Weight: "Eh",
-            Height: "Tall",
-            Body: "Lanky and broad-shouldered. Standard beagle coloration.",
-            Clothing: "(WIP) A yellow and green beanie. His shirt has the CS2 logo. He prefers to wear dark colors.",
-            Personality: "Swears a lot."
-        },
-        paragraphs: [
-            "Works part-time making donuts. Lives in a frat."
-        ]
+        paragraphs: []
     },
     'cacao': {
         name: "Cacao Clifton",
@@ -44,41 +23,6 @@ const characterInfo = {
         paragraphs: [
             "Cacao does not pull punches. She insists on ordering first at restaurants. She must be the one to hold the door open for the other person. And she absolutely is the one who picks out this week's groceries' chip flavour. Why does she do this? In her words, \"Some of us like to be getting stuff done!\"",
             "When she has trouble sleeping, she likes a little midnight hot chocolate. Her favourite food is a fried chicken sandwich with pickles and mayo. Also, she's snuggly."
-        ]
-    },
-    'stella': {
-        name: "Stella Oei",
-        imgs: ["stella_shark.png"],
-        table_data: {
-            Species: "Thresher Shark + Dragon",
-            Identity: "Woman",
-            Orientation: "Asexual, Romantic",
-            Weight: "Big",
-            Height: "Very short",
-            Body: "Pink body, deep blue eyes/inner mouth, scales along tail. Broad shoulders.",
-            Clothing: "Overalls and gloves (when working).",
-            Personality: "Loving."
-        },
-        paragraphs: [
-            "She lives on her space farm. Though she seems innocent, she's also a kleptomaniac, and steals donuts.",
-            "Her last name is pronounced \"Wei\" [wɛɪ]."
-        ]
-    },
-    'mizli': {
-        name: "Mizli Oei",
-        imgs: ["mizli.png"],
-        table_data: {
-            Species: "Thresher Shark + Dragon",
-            Identity: "Transfemme",
-            Orientation: "Asexual, Romantic",
-            Weight: "Big",
-            Height: "Very short",
-            Body: "Pear-shaped with narrow shoulders.",
-            Clothing: "TBD",
-            Personality: "TBD"
-        },
-        paragraphs: [
-            "Stella's younger sister. Her name is pronounced MITS-lee [mɪt͡sliː]."
         ]
     }
 };

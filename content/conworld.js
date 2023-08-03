2d3dba33a8cba46a57612274420d43851c99a49b
kitsumitsu <75032462+BlueBunnex@users.noreply.github.com>
2023-08-03 15:45:15 -0400

    animacy

--- content/conworld.js
+++ content/conworld.js
@@ -8,31 +8,6 @@ const cabuLanguage = `
 </div>
 
 <div class="section">
-    <h1>Phonology</h1>
-    <div>
-        <table style="width: 50%; text-align: center;">
-            <tbody>
-                <tr>
-                    <th></th>
-                    <th>Front</th>
-                    <th>Back</th>
-                </tr>
-                <tr>
-                    <th>Close</th>
-                    <td></td>
-                    <td></td>
-                </tr>
-                <tr>
-                    <th>Open</th>
-                    <td></td>
-                    <td></td>
-                </tr>
-            </tbody>
-        </table>
-    </div>
-</div>
-
-<div class="section">
     <h1>Syntax & Morphology</h1>
     <div>
         <h1>General</h1>
@@ -40,18 +15,23 @@ const cabuLanguage = `
 
         <h1>Word classes</h1>
         <ul>
-            <li><a href="#noun-morph">Nouns</a> (open, with a multitude of derivation techniques)</li>
-            <li><a href="#pronoun-morph">Pronouns</a> (open)</li>
-            <li><a href="#verb-morph">Verbs</a> (closed)</li>
-            <li><a href="#preverb-morph">Preverbs</a> (closed)</li>
-            <li><a href="#pos-morph">Prepositions</a> (closed)</li>
-            <li><a href="#adj-morph">Adjectives</a> (open, are commonly formed through <a href="https://en.wikipedia.org/wiki/Conversion_(word_formation)#:~:text=In%20linguistics%2C%20conversion%2C%20also%20called,say%2C%20derivation%20using%20only%20zero.">conversion</a>)</li>
+            <li><a href="#noun-morph">Nouns</a></li>
+            <li><a href="#pronoun-morph">Pronouns</a></li>
+            <li><a href="#verb-morph">Verbs</a></li>
+            <li><a href="#preverb-morph">Preverbs</a></li>
+            <li><a href="#pos-morph">Prepositions</a></li>
+            <li><a href="#adj-morph">Adjectives</a></li>
             <li>...</li>
         </ul>
         
         <h1 id="#noun-morph">Nouns</h1>
+        <p>Nouns are either animate or inanimate.</p>
+
         <h1 id="#pronoun-morph">Pronouns</h1>
+        
         <h1 id="#verb-morph">Verbs</h1>
+        <!-- https://en.wikipedia.org/wiki/Direct–inverse_alignment -->
+        
         <h1 id="#preverb-morph">Preverbs</h1>
         <h1 id="#pos-morph">Prepositions</h1>
         <h1 id="#adj-morph">Adjectives</h1>

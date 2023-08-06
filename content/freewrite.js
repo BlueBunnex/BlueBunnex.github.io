e51c06940c2dd84462845e585242e411cf1bbada
kitsumitsu <75032462+BlueBunnex@users.noreply.github.com>
2023-08-06 16:09:31 -0400

    I think that's how dictionaries work

--- content/freewrite.js
+++ content/freewrite.js
@@ -5,7 +5,7 @@
 // ige = they say
 
 const stories = {
-"sandland" = `
+"sandland": `
 <p>
 So, problem. My eyes have stopped working and I'm letting it get to me. Okay, I'm really letting it get to me, I'm stood in the middle of the living room and I'm holding my arms out in front of me and, no, nothing there, and I slowly swing them to either side and, yup, nothing there either. The couch must be, uhm, <i>somewhere</i> around here, I don't remember which direction I was looking when everything suddenly stopped being visible.
 </p><p>

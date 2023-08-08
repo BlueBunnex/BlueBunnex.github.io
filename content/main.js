974fe33ece3337d6aa4ee2a6fad4a4669954616a
kitsumitsu <75032462+BlueBunnex@users.noreply.github.com>
2023-08-08 16:05:56 -0400

    update display

--- content/main.js
+++ content/main.js
@@ -1,25 +1,14 @@
 let mainConstruct = `
-<div class="section">
-    <h1>me lore</h1>
-    <h2>#000</h2>
-    <div>
-        <p>some information regarding me and my fursona.</p>
-        <a class="ref" href="javascript:loadContent(me);">check it out</a>
-    </div>
-</div>
 
-<div class="section">
-    <h1>the kitsumitsuverse</h1>
-    <h2>#001</h2>
-    <div>
-        <p>aka my hot hot ocs</p>
-        <a class="ref" href="javascript:loadContent(universe);">check it out</a>
-    </div>
-</div>
+<h1 class="click-header" onclick="loadContent(me);">My Fursona</h1>
+
+<h1 class="click-header" onclick="loadContent(universe);">Original Characters</h1>
+
+<h1 class="click-header" onclick="loadContent(assets);">Asset Downloads</h1>
 
 <div class="section">
     <h1>conworlds, conlangs, conspecies, oh my!</h1>
-    <h2>#002</h2>
+    <h2>WIP display</h2>
     <div>
         <p>I'm trying to build together a little fantasy world that features (at least) one conlang for me to work on, since I'm a linguist (who enjoys writing)! I'm thinking gothic sci-fi mecha fantasy.</p>
         <a class="ref" href="javascript:loadContent(cabuLanguage);">Cabü (conlang)</a>
@@ -29,19 +18,10 @@ let mainConstruct = `
 
 <div class="section">
     <h1>freewrites</h1>
-    <h2>#003</h2>
+    <h2>WIP display</h2>
     <div>
         <p>CW: swearing</p>
         REPLACE_CHAPTERS
-    </div>
-</div>
-
-<div class="section">
-    <h1>asset downloads</h1>
-    <h2>#004</h2>
-    <div>
-        <p>some assets that you can use if you like, mostly 3D stuff. they're all free and you don't have to give credit, but you know, you can!</p>
-        <a class="ref" href="javascript:loadContent(assets);">check it out</a>
     </div>
 </div>
 `;

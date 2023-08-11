c795e751cf5e3cf5d0605cbefd741f7f2ef96fce
kitsumitsu <75032462+BlueBunnex@users.noreply.github.com>
2023-08-10 23:10:31 -0400

    update to table data

--- content/characters.js
+++ content/characters.js
@@ -2,9 +2,14 @@ const characterInfo = {
     'pipim': {
         name: "Pipim",
         imgs: [],
-        table_data: {},
+        table_data: {
+            "heavy pink lipstick",
+            "pink sweater with white undershirt",
+            "voluminous blonde hair",
+            "energetic and cuddly"
+        },
         paragraphs: [
-            "bright eyes, pink blush, heavy pink lipstick, pink sweater with white undershirt, long voluminous blonde hair, energetic and cuddly"
+            
         ]
     }
 };

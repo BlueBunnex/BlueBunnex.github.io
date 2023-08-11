4957dc433b628714e56e8c88ef522a20ab6ba21b
kitsumitsu <75032462+BlueBunnex@users.noreply.github.com>
2023-08-10 23:33:05 -0400

    lore!!

--- content/characters.js
+++ content/characters.js
@@ -3,11 +3,11 @@ const characterInfo = {
         name: "Pipim",
         imgs: [],
         table_data: {
-            "Appearance": "voluminous blonde hair, pink sweater with white undershirt, heavy pink lipstick",
+            "Appearance": "voluminous blonde hair; pink sweater with white undershirt; heavy pink lipstick; very tall",
             "Personality": "energetic and cuddly"
         },
         paragraphs: [
-            
+            "Pipim lives deep in the city, in a country where it rains a lot."
         ]
     }
 };
